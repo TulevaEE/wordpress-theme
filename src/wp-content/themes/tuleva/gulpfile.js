8abf3e4dc66c2bb7d37e258b459239a1dc1c0019
Carl Rannaberg <carl@pipedrive.com>
2017-04-22 20:35:31 +0300

    Added gulp css building.

--- src/wp-content/themes/tuleva/gulpfile.js
+++ src/wp-content/themes/tuleva/gulpfile.js
@@ -1,5 +1,35 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sourcemaps = require('gulp-sourcemaps'),
+    gulpif = require('gulp-if'),
+    sass = require('gulp-sass'),
+    cleanCSS = require('gulp-clean-css'),
+    autoprefixer = require('gulp-autoprefixer'),
+    watch = require('gulp-watch'),
+    util = require('gulp-util'),
+    isCompressed = false,
+    cssDir = 'css',
+    sassSrc = 'sass/*.scss',
+    error = function(err) {
+        if (err) {
+            util.log(err.toString());
+        }
+
+        if (this.emit) {
+            this.emit('end');
+        }
+    };
 
 gulp.task('default', function() {
-  // place code for your default task here
+  return gulp.src(sassSrc)
+    .pipe(watch(sassSrc))
+    .pipe(gulpif(!isCompressed, sourcemaps.init()))
+    .pipe(sass({ outputStyle: isCompressed ? 'compressed' : 'normal' })
+                    .on('error', error))
+    .pipe(gulpif(isCompressed, cleanCSS({level: 2})))
+    .pipe(autoprefixer({
+        browsers: ['> 1%', 'last 2 versions', 'safari >= 7'],
+        cascade: false
+    }))
+    .pipe(gulpif(!isCompressed, sourcemaps.write('.')))
+    .pipe(gulp.dest(cssDir));
 });
