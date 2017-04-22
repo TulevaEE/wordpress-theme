e1ddb2137f9cb63db1c272ff28dab54495f3b61c
Carl Rannaberg <carl@pipedrive.com>
2017-04-22 20:48:45 +0300

    Added production building and pre-commit task.

--- src/wp-content/themes/tuleva/gulpfile.js
+++ src/wp-content/themes/tuleva/gulpfile.js
@@ -6,7 +6,6 @@ var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     watch = require('gulp-watch'),
     util = require('gulp-util'),
-    isCompressed = false,
     cssDir = 'css',
     sassSrc = 'sass/*.scss',
     error = function(err) {
@@ -17,19 +16,28 @@ var gulp = require('gulp'),
         if (this.emit) {
             this.emit('end');
         }
-    };
+    },
+    buildCss = function(isCompressed) {
+        return gulp.src(sassSrc)
+            .pipe(watch(sassSrc))
+            .pipe(gulpif(!isCompressed, sourcemaps.init()))
+            .pipe(sass({ outputStyle: isCompressed ? 'compressed' : 'normal' })
+                            .on('error', error))
+            .pipe(gulpif(isCompressed, cleanCSS({level: 2})))
+            .pipe(autoprefixer({
+                browsers: ['> 1%', 'last 2 versions', 'safari >= 7'],
+                cascade: false
+            }))
+            .pipe(gulpif(!isCompressed, sourcemaps.write('.')))
+            .pipe(gulp.dest(cssDir));
+    }
 
 gulp.task('default', function() {
-  return gulp.src(sassSrc)
-    .pipe(watch(sassSrc))
-    .pipe(gulpif(!isCompressed, sourcemaps.init()))
-    .pipe(sass({ outputStyle: isCompressed ? 'compressed' : 'normal' })
-                    .on('error', error))
-    .pipe(gulpif(isCompressed, cleanCSS({level: 2})))
-    .pipe(autoprefixer({
-        browsers: ['> 1%', 'last 2 versions', 'safari >= 7'],
-        cascade: false
-    }))
-    .pipe(gulpif(!isCompressed, sourcemaps.write('.')))
-    .pipe(gulp.dest(cssDir));
+  return buildCss(false);
 });
+
+gulp.task('production', function() {
+  return buildCss(true);
+});
+
+gulp.task('pre-commit', ['production']);
