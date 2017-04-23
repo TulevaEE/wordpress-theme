ac4304172cade50fc1eed8187ae6602fc9f6cbad
Carl Rannaberg <carl@pipedrive.com>
2017-04-24 01:45:49 +0300

    Added 404 page.

---
+++ src/wp-content/themes/tuleva/404.php
@@ -0,0 +1,3 @@
+<?php get_header(); ?>
+<?php get_template_part( 'templates/404' ); ?>
+<?php get_footer(); ?>
---
+++ src/wp-content/themes/tuleva/templates/404.php
@@ -0,0 +1,10 @@
+<div class="page-container">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="page-title">Pole olemas sellist lehekülge</h1>
+                <h2>Ehk läks aadressi sisestamisega midagi valesti?</h2>
+            </div>
+        </div>
+    </div>
+</div>
