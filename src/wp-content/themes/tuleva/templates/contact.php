d74603a6139fc63a97b621a576942c7f756bccc3
Carl Rannaberg <carl@pipedrive.com>
2017-04-24 01:53:20 +0300

    Contact blocks improvement.

--- src/wp-content/themes/tuleva/templates/contact.php
+++ src/wp-content/themes/tuleva/templates/contact.php
@@ -10,7 +10,7 @@
                     <div class="contacts-block">
                         <div class="col-md-4 contacts-block__item">
                             <img class="contacts-block__image" src="<?php echo get_template_directory_uri() ?>/img/tonu-pekk.png" alt="Tõnu Pekk">
-                            <h2>Tõnu Pekk</h2>
+                            <h2 class="contacts-block__title">Tõnu Pekk</h2>
                             <div class="contacts-block__row">
                                 <a href="mailto:tonu.pekk@tuleva.ee">tonu.pekk@tuleva.ee</a>
                             </div>
@@ -18,7 +18,7 @@
                         </div>
                         <div class="col-md-4 contacts-block__item">
                             <img class="contacts-block__image" src="<?php echo get_template_directory_uri() ?>/img/priit-lepasepp.png" alt="Priit Lepasepp">
-                            <h2>Priit Lepasepp</h2>
+                            <h2 class="contacts-block__title">Priit Lepasepp</h2>
                             <div class="contacts-block__row">
                                 <a href="mailto:priit.lepasepp@tuleva.ee">priit.lepasepp@tuleva.ee</a>
                             </div>
@@ -26,7 +26,7 @@
                         </div>
                         <div class="col-md-4 contacts-block__item">
                             <img class="contacts-block__image" src="<?php echo get_template_directory_uri() ?>/img/kristi-saare.png" alt="Kristi Saare">
-                            <h2>Kristi Saare</h2>
+                            <h2 class="contacts-block__title">Kristi Saare</h2>
                             <div class="contacts-block__row">
                                 <a href="mailto:kristi.saare@tuleva.ee">kristi.saare@tuleva.ee</a>
                             </div>
