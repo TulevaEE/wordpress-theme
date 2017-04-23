f78b0d480bf956797d97c39581d435d8953348cf
Carl Rannaberg <carl@pipedrive.com>
2017-04-24 01:00:28 +0300

    Footer.

--- src/wp-content/themes/tuleva/footer.php
+++ src/wp-content/themes/tuleva/footer.php
@@ -1,3 +1,51 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 footer_column">
+                <h4 class="footer__title">Liitumistasu Konto</h4>
+                <div class="footer__column__text">
+                    <div class="footer__column__text__row">Tulundusühistu Tuleva</div>
+                    <div class="footer__column__text__row">EE132200221064032136</div>
+                    <div class="footer__column__text__row">Swedbank</div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <h4 class="footer__title">Tulundusühistu Tuleva</h4>
+                <div class="footer__column__text">
+                    <div class="footer__column__text__row">Telliskivi 60, Tallinn, 10412</div>
+                    <div class="footer__column__text__row">+372 644 5100</div>
+                    <div class="footer__column__text__row">Reg. kood: 14041764</div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <h4 class="footer__title">Tuleva Fondid AS</h4>
+                <div class="footer__column__text">
+                    <div class="footer__column__text__row">Telliskivi 60, Tallinn, 10412</div>
+                    <div class="footer__column__text__row">+372 644 5100</div>
+                    <div class="footer__column__text__row">Tegevusloa nr: 4.1-1/25</div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <a href="#" class="footer__language">In English</a>
+                <div class="footer__social">
+                    <a href="#">
+                        <img src="<?php echo get_template_directory_uri() ?>/img/icon-facebook.svg" alt="Facebook">
+                    </a>
+                    <a href="#">
+                        <img src="<?php echo get_template_directory_uri() ?>/img/icon-twitter.svg" alt="Twitter">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 footer__text">
+                <p>Enne kui otsustad, millisesse pensionifondi oma II samba osakud ja tulevased sissemaksed suunata, tutvu kindlasti olulise infoga. Meie pensionifondide tingimused on <a href="#">siin</a>, võrdluseks leiad kõigi Eesti pensionifondide info <a href="#">pensionikeskusest</a>. Palun pane tähele, et Tuleva ühistu, Tuleva fondivalitseja ega Tuleva töötajad ei anna sinule investeerimisnõu ega saakski anda, sest me ei ole taotlenud Investeerimisnõustamise tegevusluba. Anname alati endast parima, et jagada sinuga ausalt ja arusaadavalt infot, mida arvesse võttes saad ise otsustada. Kui sul on küsimusi, võta meiega ühendust või konsulteeri erapooletute asjatundjatega Pensionikeskusese <a href="#">infotelefonil</a> või Finantsinspektsiooni <a href="#">rahatarkuse lehel</a>.</p>
+                <p>Tuleva kodulehe <a href="#">kasutustingimused</a>.</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
 <?php wp_footer(); ?>
 
 </body>
