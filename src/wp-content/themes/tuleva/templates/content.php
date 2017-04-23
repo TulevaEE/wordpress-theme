8bc74f25a96b828a2b9c9e0d7f7fc48f8bbd8148
Carl Rannaberg <carl@pipedrive.com>
2017-04-23 23:38:28 +0300

    Content styles.

--- src/wp-content/themes/tuleva/templates/content.php
+++ src/wp-content/themes/tuleva/templates/content.php
@@ -1,7 +1,18 @@
-<?php if (get_the_title()) { ?>
-    <?php the_title(); ?>
-<?php } ?>
-
-<?php if (get_the_content()) { ?>
-    <?php the_content(); ?>
-<?php } ?>
+<div class="page-container">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+                    <?php if (get_the_title()) { ?>
+                        <h1 class="page-title"><?php the_title(); ?></h1>
+                    <?php } ?>
+                    <div class="content">
+                        <?php if (get_the_content()) { ?>
+                            <?php the_content(); ?>
+                        <?php } ?>
+                    </div>
+                <?php endwhile; ?>
+            </div>
+        </div>
+    </div>
+</div>
