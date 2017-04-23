bb7d6f552514b2b89398ed90be255bdf3f695651
Carl Rannaberg <carl@pipedrive.com>
2017-04-23 23:43:42 +0300

    Content width tuning.

--- src/wp-content/themes/tuleva/templates/content.php
+++ src/wp-content/themes/tuleva/templates/content.php
@@ -1,7 +1,7 @@
 <div class="page-container">
     <div class="container">
         <div class="row">
-            <div class="col-md-10 col-md-offset-1">
+            <div class="col-md-8 col-md-offset-2">
                 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
                     <?php if (get_the_title()) { ?>
                         <h1 class="page-title"><?php the_title(); ?></h1>
