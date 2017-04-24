53ac9257448b376a81a46e72b6cf1daaba36913c
Carl Rannaberg <carl@pipedrive.com>
2017-04-25 00:45:48 +0300

    Added media blocks.

--- src/wp-content/themes/tuleva/templates/fund.php
+++ src/wp-content/themes/tuleva/templates/fund.php
@@ -1,12 +1,12 @@
 <div class="page-container">
     <div class="container">
         <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-        <div class="row">
+        <div class="row row-spacing-bottom">
             <div class="col-md-12">
-                <h1 class="h1-sm h1-bottom-margin text-center">Tuleva Fondid AS on Eesti esimene ja ainus inimeste endi pensionifondi valitseja. Tee meie edulugu enda omaks!</h1>
+                <h1 class="h1-sm text-center">Tuleva Fondid AS on Eesti esimene ja ainus inimeste endi pensionifondi valitseja. Tee meie edulugu enda omaks!</h1>
             </div>
         </div>
-        <div class="row text-boxes">
+        <div class="row row-spacing text-boxes">
             <div class="col-md-6">
                 <div class="text-box">
                     <h2 class="text-box__title">Mida me sulle lubame?</h2>
@@ -21,6 +21,44 @@
                 <div class="text-box">
                     <h2 class="text-box__title">Mida me sulle ei luba?</h2>
                     <p>Muinasjutulist tootlust. Maailmamajanduse käekäik pole meie teha. Meie teha on, et suur osa sinu säästudest ei kuluks ekstravagantsetele reklaamikampaaniatele ja muudele tegevustele, mis sinule väärtust ei loo.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row row-spacing">
+            <div class="col-md-12">
+                <div class="media">
+                    <div class="media-body">
+                        <h2 class="media-heading">Tuleva Maailma Aktsiate Pensionifond</h2>
+                        <ul>
+                            <li>73% fondi paigutatud rahast investeeritakse maailma aktsiaturu indeksit (MSCI AWCI) järgivatesse fondidesse ja 27% maailma võlakirjaturu indeksit (Barclays Global Aggregate Bond Index) järgivatesse fondidesse</li>
+                            <li>Fondi strateegia on agressiivne</li>
+                            <li>Fondi riskitase on keskmine</li>
+                            <li>Fondi NAVi muutusete graafiku leiad siit: (graafik).</li>
+                            <li>Fondi kogukulumäär on XXX. See koosneb haldustasust xx% ja tehingu ning allfondide kulust YY%</li>
+                        </ul>
+                    </div>
+                    <div class="media-right">
+                        <img class="media-object" src="<?php echo get_template_directory_uri() ?>/img/aktsiafond-pie.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row row-spacing">
+            <div class="col-md-12">
+                <div class="media">
+                    <div class="media-left">
+                        <img class="media-object" src="<?php echo get_template_directory_uri() ?>/img/volakirjad-pie.svg" alt="">
+                    </div>
+                    <div class="media-body">
+                        <h2 class="media-heading">Tuleva Maailma Võlakirjade Pensionifond</h2>
+                        <ul>
+                            <li>100% fondi paigutatud rahast investeeritakse maailma võlakirjaturu indeksit (Barclays Global Aggregate Bond Index) järgivatesse fondidesse</li>
+                            <li>Fondi strateegia on konservatiivne</li>
+                            <li>Fondi riskitase on madal</li>
+                            <li>Fondi NAVi muutusete graafiku leiad siit: (graafik).</li>
+                            <li>Fondi kogukulumäär on XXX. See koosneb haldustasust xx% ja tehingu ning allfondide kulust YY%</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
