bba7a8130562ba091100b7cdb958acb7e235dc8c
Carl Rannaberg <carl@pipedrive.com>
2017-04-26 00:53:12 +0300

    Added social and recent articles components.

--- src/wp-content/themes/tuleva/templates/single-content.php
+++ src/wp-content/themes/tuleva/templates/single-content.php
@@ -13,11 +13,32 @@
                 <div class="post-meta">
                     <a href="#" class="post-meta__author">Tõnu Pekk</a>  11. aprill 2017
                 </div>
-                <?php if (get_the_content()) { ?>
-                <div class="content">
-                    <?php the_content(); ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-1 col-md-offset-1">
+                <div class="post-social">
+                    <a href="#">
+                        <img src="<?php echo get_template_directory_uri() ?>/img/icon-facebook-gray.svg" alt="Facebook">
+                    </a>
+                    <a href="#">
+                        <img src="<?php echo get_template_directory_uri() ?>/img/icon-twitter-gray.svg" alt="Twitter">
+                    </a>
                 </div>
+            </div>
+            <div class="col-md-8">
+                <?php if (get_the_content()) { ?>
+                    <div class="content">
+                        <?php the_content(); ?>
+                    </div>
                 <?php } ?>
+                <div class="recent-articles">
+                    <h5 class="recent-articles__heading">Eelmised artiklid</h5>
+                    <ul class="recent-articles__list">
+                        <li><a href="#">Valmis tegime! Tuleva käivitab II samba pensionifondid valitsemistasuga 0,34%</a></li>
+                        <li><a href="#">Õiguskantsler: II samba paindlikkust tuleks suurendada kindlustusfirmade kasumi arvel</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
