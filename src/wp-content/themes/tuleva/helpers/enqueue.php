9dbeb4d38c07c9b175db9b8b1e9e1d02985e7b4e
Carl Rannaberg <carl@pipedrive.com>
2017-05-09 13:29:38 +0300

    Bumbed cache breaker.

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -27,7 +27,7 @@ class ThemeEnqueue {
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '03052017',
+                'ver' => '09052017',
                 'media' => 'all',
                 'enqueue' => true
             ],
