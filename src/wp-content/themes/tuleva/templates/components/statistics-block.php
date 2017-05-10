622790e8d93069f69c3e0ba97dbce23c80c83d0b
Carl Rannaberg <carl@pipedrive.com>
2017-05-10 15:36:46 +0300

    Copy change.

--- src/wp-content/themes/tuleva/templates/components/statistics-block.php
+++ src/wp-content/themes/tuleva/templates/components/statistics-block.php
@@ -6,7 +6,7 @@
                     if (!get_sub_field('has_padding')) {
                         echo ' no-padding';
                     } ?>">
-                    <div class="text-rows-box__row">Tuleva liikmeid on <strong class="text-highlight"><?php the_sub_field('members'); ?></strong> inimest.</div>
+                    <div class="text-rows-box__row">Tulevaga on liitunud <strong class="text-highlight"><?php the_sub_field('members'); ?></strong> inimest.</div>
                     <div class="text-rows-box__row">Tuleva Maailma Aktsiate Pensionifondi maht on <strong class="text-highlight"><?php echo number_format(get_sub_field('stock_market_volume'), 0, ',', ' '); ?></strong> eurot.</div>
                     <div class="text-rows-box__row">Tuleva Maailma Võlakirjade Pensionifondi maht on <strong class="text-highlight"><?php echo number_format(get_sub_field('bonds_volume'), 0, ',', ' '); ?></strong> eurot.</div>
                 </div>
