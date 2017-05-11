0ee21ed5e2a70e17016c46be545c64a6a65718a2
Carl Rannaberg <carl@pipedrive.com>
2017-05-12 00:10:35 +0300

    Bump cache breaker.

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -27,7 +27,7 @@ class ThemeEnqueue {
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '09052017',
+                'ver' => '11052017',
                 'media' => 'all',
                 'enqueue' => true
             ],
