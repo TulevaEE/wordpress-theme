8e6d2d0c7e9f4fa3edc2adac5d0a5cd4e86fcb73
Carl Rannaberg <carl@pipedrive.com>
2017-05-31 14:46:42 +0300

    Translation update.

--- src/wp-content/themes/tuleva/templates/404.php
+++ src/wp-content/themes/tuleva/templates/404.php
@@ -2,8 +2,8 @@
     <div class="container">
         <div class="row row-spacing-bottom">
             <div class="col-md-12 text-center">
-                <h1 class="page-title">Pole olemas sellist lehekülge</h1>
-                <h2>Ehk läks aadressi sisestamisega midagi valesti?</h2>
+                <h1 class="page-title"><?php _e('There is no such page', TEXT_DOMAIN); ?></h1>
+                <h2><?php _e('Maybe something got mixed up when entering the address?', TEXT_DOMAIN); ?></h2>
             </div>
         </div>
     </div>
