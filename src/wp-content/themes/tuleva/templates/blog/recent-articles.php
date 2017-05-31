57284d3b08136b02a571aa84cd16e64ace862a5a
Carl Rannaberg <carl@pipedrive.com>
2017-05-31 11:31:16 +0300

    Recent articles fix.

--- src/wp-content/themes/tuleva/templates/blog/recent-articles.php
+++ src/wp-content/themes/tuleva/templates/blog/recent-articles.php
@@ -1,12 +1,16 @@
 <?php
 global $post;
 $current_post_id = $post->ID;
+$current_post_categories = get_the_category($current_post_id);
+$current_post_cat_id = $current_post_categories[0]->term_id;
 ?>
 <div class="recent-articles">
     <h5 class="recent-articles__heading">Eelmised artiklid</h5>
     <ul class="recent-articles__list">
         <?php
             $args = [
+                        'category' => $current_post_cat_id,
+                        'suppress_filters' => 0,
                         'numberposts' => '2',
                         'exclude' => $current_post_id
                     ];
