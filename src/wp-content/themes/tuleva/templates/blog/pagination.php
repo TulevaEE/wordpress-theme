6e1a17a5fe5c38a770c4867c47772130b8824270
Carl Rannaberg <carl@pipedrive.com>
2017-06-01 13:44:52 +0300

    Translation update.

--- src/wp-content/themes/tuleva/templates/blog/pagination.php
+++ src/wp-content/themes/tuleva/templates/blog/pagination.php
@@ -1,10 +1,11 @@
 <?php if ($paged > 1) { ?>
     <div class="pagination">
-        <div class="pagination__previous"><?php next_posts_link('Vanemad artiklid'); ?></div>
-        <div class="pagination__next"><?php previous_posts_link('Uuemad artiklid'); ?></div>
+        <div class="pagination__previous"><?php next_posts_link(__('Older articles', TEXT_DOMAIN)); ?></div>
+        <div class="pagination__next"><?php previous_posts_link(__('Newer articles', TEXT_DOMAIN)); ?></div>
     </div>
 <?php } else { ?>
     <div class="pagination">
-        <div class="pagination__previous"><?php next_posts_link('Vanemad artiklid'); ?></div>
+        <?php _e('Older articles'); ?>
+        <div class="pagination__previous"><?php next_posts_link(__('Older articles', TEXT_DOMAIN)); ?></div>
     </div>
 <?php } ?>
