de04e6c926b8535da8486a24dfee96f98c19af2a
Erko Risthein <erko.risthein@transferwise.com>
2017-06-06 11:44:39 +0300

    Added optimizely

--- src/wp-content/themes/tuleva/templates/head.php
+++ src/wp-content/themes/tuleva/templates/head.php
@@ -3,5 +3,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="description" content="<?php bloginfo('name'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://cdn.optimizely.com/js/8423232854.js"></script>
     <?php wp_head(); ?>
 </head>
