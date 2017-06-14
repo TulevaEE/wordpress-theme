1eea1c7aee4ed20e6f1b317a8c8cd20002c704e6
Erko Risthein <erko.risthein@transferwise.com>
2017-06-14 12:53:19 +0300

    Added banks scss

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -27,7 +27,7 @@ class ThemeEnqueue {
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '12062017',
+                'ver' => '14062017',
                 'media' => 'all',
                 'enqueue' => true
             ],
