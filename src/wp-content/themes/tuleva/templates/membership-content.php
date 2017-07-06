93262a049c1a99db50014baa9b65e6d36b2f1717
jordan.valdma <jordan.valdma@gmail.com>
2017-07-06 18:31:18 +0300

    Updates version with login configuration fixes

--- src/wp-content/themes/tuleva/templates/membership-content.php
+++ src/wp-content/themes/tuleva/templates/membership-content.php
@@ -9,7 +9,7 @@
                     <div>
                         <div id="inline-login" class="inline-login"></div>
                         <link href="<?php echo get_template_directory_uri(); ?>/templates/onboarding-client/static/css/login.efe556b6.css" rel="stylesheet">
-                        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/templates/onboarding-client/static/js/login.e6f60d66.js"></script>
+                        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/templates/onboarding-client/static/js/login.5133bfa1.js"></script>
                         <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/templates/onboarding-client/static/js/polyfills.8b285ffa.js"></script>
                         <!-- <script type="text/javascript" src="http://localhost:3000/static/js/login.js"></script> -->
                         <!-- <script type="text/javascript" src="http://localhost:3000/static/js/polyfills.js"></script> -->
