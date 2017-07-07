d88d67fcbb5f21a10fa08871a2cd076f901ffbba
jordan.valdma <jordan.valdma@gmail.com>
2017-07-07 14:57:53 +0300

    Adds funds page hero block

--- src/wp-content/themes/tuleva/templates/fund-content.php
+++ src/wp-content/themes/tuleva/templates/fund-content.php
@@ -1,12 +1,55 @@
 <div class="page-container">
-    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-    <div class="container row-spacing">
-        <div class="row">
-            <div class="col-md-12">
-                <h1 class="h1-sm text-center"><?php the_field('heading'); ?></h1>
+    <div class="page-container funds-container">    
+        <div class="page-container funds-container-hero">  
+
+            <div class="funds-container-hero-gradient">    
+                <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+                <div class="container row-spacing">
+                    <!--
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h1 class="h1-sm text-center"><?php the_field('heading'); ?></h1>
+                        </div>
+                    </div>
+                    -->
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h1 class="h1-sm text-center"><?php _e('Tuleva asutajad ja liikmed koguvad juba II sambasse Tuleva pensionifondis.'); ?></h1>
+                        </div>
+                    </div>
+                    <div class="row mt-5 top-buffer">
+                        <div class="col-md-12">
+                            <p class="h1-sm text-center"><?php _e('Meiega koos saavad pensioni koguda kõik Eesti inimesed. Pensionifondi vahetus on tasuta ja võtab netipangas 5 minutit.') ?></p>
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-md-12 text-center">
+                            <a href="#" class="btn btn-primary btn-lg">
+                                <?php _e('Kuidas tuua pension Tulevasse?') ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
+
         </div>
     </div>
+
+
+<!--     <div>
+        <div class="row">
+            <div class="col-md-4">
+                <h1 class="h1-sm text-center"><?php _e('Kuidas fondi valida?'); ?></h1>
+            </div>
+            <div class="col-md-4">
+                <h1 class="h1-sm text-center"><?php _e('Kas Tuleva on turvaline?'); ?></h1>
+            </div>
+            <div class="col-md-4">
+                <h1 class="h1-sm text-center"><?php _e('Miks kulud loevad?'); ?></h1>
+            </div>
+        </div>
+    </div> -->
+
 
     <?php
         if (have_rows('fund_components')) {
@@ -30,5 +73,29 @@
         }
     ?>
 
+
+<!--
+    <?php
+        if (have_rows('fund_components')) {
+            while (have_rows('fund_components')) { the_row();
+                if (get_row_layout() === 'text_boxes') {
+                    get_template_part('templates/components/text-boxes');
+                } elseif (get_row_layout() === 'button') {
+                    get_template_part('templates/components/button');
+                } elseif (get_row_layout() === 'media') {
+                    get_template_part('templates/components/media');
+                } elseif (get_row_layout() === 'text_rows_block') {
+                    get_template_part('templates/components/text-rows-block');
+                } elseif (get_row_layout() === 'comparison_block') {
+                    get_template_part('templates/components/comparison-block');
+                } elseif (get_row_layout() === 'single_quote_block') {
+                    get_template_part('templates/components/single-quote-block');
+                } elseif (get_row_layout() === 'fund_contacts_block') {
+                    get_template_part('templates/components/fund-contacts-block');
+                }
+            }
+        }
+    ?>
+-->
     <?php endwhile; ?>
 </div>
