158c3707c3bf82d05e4be308719619e594b00139
Erko Risthein <erko@risthein.ee>
2017-07-12 16:12:35 +0300

    Added the security block to the funds page

---
+++ src/wp-content/themes/tuleva/templates/components/security.php
@@ -0,0 +1,45 @@
+<div class="container">
+    <div class="row row-spacing-half">
+        <div class="col-md-offset-1 col-md-10">
+            <h1 class="h1-sm text-center">
+                <?php _e('Kas meie raha on Tulevas ikka sama hästi kaitstud kui panga pensionifondis?') ?>
+            </h1>
+            <h1 class="text-center color-intense"><?php _e('JAH!') ?></h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="security-block">
+                <div class="security-block__icon">
+                    <img src="<?php echo get_template_directory_uri() ?>/img/icon-umbrella.svg" alt="Umbrella" />
+                </div>
+                <div class="security-block__content">
+                    <h4><?php _e('Finantsinspektsioon') ?></h4>
+                    <p><?php _e('andis Tuleva fondivalitsejale tegevusloa ja valvab, et meie igapäevane tegevus vastaks igati nõudmistele.') ?></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="security-block">
+                <div class="security-block__icon">
+                    <img src="<?php echo get_template_directory_uri() ?>/img/icon-safe.svg" alt="Safe" />
+                </div>
+                <div class="security-block__content">
+                    <h4><?php _e('Swedbank') ?></h4>
+                    <p><?php _e('on Tuleva fondide depoopank. Depoopank kinnitab iga tehingu fondi varaga. Täpselt nii nagu panga enda fondide puhul.') ?></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="security-block">
+                <div class="security-block__icon">
+                    <img src="<?php echo get_template_directory_uri() ?>/img/icon-lifesaver.svg" alt="Lifesaver" />
+                </div>
+                <div class="security-block__content">
+                    <h4><?php _e('Riiklik tagatisfond') ?></h4>
+                    <p><?php _e('kaitseb kõigi pensionifondide investoreid halvima vastu, kui fondivalitseja süül peaks tekkima kahju.') ?></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- src/wp-content/themes/tuleva/templates/fund-content.php
+++ src/wp-content/themes/tuleva/templates/fund-content.php
@@ -56,6 +56,7 @@
             while (have_rows('fund_components')) { the_row();
                 if (get_row_layout() === 'text_boxes') {
                     get_template_part('templates/components/text-boxes');
+                    get_template_part('templates/components/security');
                 } elseif (get_row_layout() === 'button') {
                     get_template_part('templates/components/button');
                 } elseif (get_row_layout() === 'media') {
@@ -73,29 +74,5 @@
         }
     ?>
 
-
-<!--
-    <?php
-        if (have_rows('fund_components')) {
-            while (have_rows('fund_components')) { the_row();
-                if (get_row_layout() === 'text_boxes') {
-                    get_template_part('templates/components/text-boxes');
-                } elseif (get_row_layout() === 'button') {
-                    get_template_part('templates/components/button');
-                } elseif (get_row_layout() === 'media') {
-                    get_template_part('templates/components/media');
-                } elseif (get_row_layout() === 'text_rows_block') {
-                    get_template_part('templates/components/text-rows-block');
-                } elseif (get_row_layout() === 'comparison_block') {
-                    get_template_part('templates/components/comparison-block');
-                } elseif (get_row_layout() === 'single_quote_block') {
-                    get_template_part('templates/components/single-quote-block');
-                } elseif (get_row_layout() === 'fund_contacts_block') {
-                    get_template_part('templates/components/fund-contacts-block');
-                }
-            }
-        }
-    ?>
--->
     <?php endwhile; ?>
 </div>
