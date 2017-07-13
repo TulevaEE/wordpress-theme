5275ffda79e29841a82ffc484363204debfce31e
Erko Risthein <erko@risthein.ee>
2017-07-13 13:14:31 +0300

    Implemented the funds page navigation

---
+++ src/wp-content/themes/tuleva/templates/components/fund-navigation.php
@@ -0,0 +1,37 @@
+<div class="container">
+    <div class="row row-spacing-half text-center">
+        <div class="col-sm-4">
+            <a class="fund-navigation" href="#">
+                <h4><?php _e('Kuidas fondi valida?'); ?></h4>
+                <div class="fund-navigation__arrow-wrapper">
+                    <div class="fund-navigation__arrow-container">
+                        <img class="fund-navigation__arrow" src="http://uploads.webflow.com/595f2918e1bb7d77368ed44e/596239e234d7a523f4e66e1c_icon-arrow-right.svg" alt="Arrow" />
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-sm-4">
+            <a class="fund-navigation" href="#">
+                <h4><?php _e('Kas Tuleva on turvaline?'); ?></h4>
+                <div class="fund-navigation__arrow-wrapper">
+                    <div class="fund-navigation__arrow-container">
+                        <img class="fund-navigation__arrow" src="http://uploads.webflow.com/595f2918e1bb7d77368ed44e/596239e234d7a523f4e66e1c_icon-arrow-right.svg" alt="Arrow" />
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-sm-4">
+            <a class="fund-navigation" href="#">
+                <h4><?php _e('Miks kulud loevad?'); ?></h4>
+                <div class="fund-navigation__arrow-wrapper">
+                    <div class="fund-navigation__arrow-container">
+                        <img class="fund-navigation__arrow" src="http://uploads.webflow.com/595f2918e1bb7d77368ed44e/596239e234d7a523f4e66e1c_icon-arrow-right.svg" alt="Arrow" />
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="row">
+        <hr />
+    </div>
+</div>
