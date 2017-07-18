5d40058ef987cc7edfa247772546ae88210cefcf
Erko Risthein <erko@risthein.ee>
2017-07-18 12:25:11 +0300

    Fixed quotation marks in translations

--- src/wp-content/themes/tuleva/templates/components/fund-choosing.php
+++ src/wp-content/themes/tuleva/templates/components/fund-choosing.php
@@ -12,10 +12,10 @@
                     <img class="row-spacing-quarter" src="<?php echo get_template_directory_uri() ?>/img/aktsiafond-pie.svg" alt="<?php _e('Tuleva World Stocks Pension Fund', TEXT_DOMAIN); ?>"/>
                 </div>
                 <div class="text-box__content">
-                    <p><?php _e('The equity fund with lowest costs in Estonia:<br />management fee <span class=“text-highlight”><strong>0,34%</strong></span> and total expense ratio <span class=“text-highlight”><strong>0,5%</strong></span>', TEXT_DOMAIN); ?></p>
+                    <p><?php _e('The equity fund with lowest costs in Estonia:<br />management fee <span class="text-highlight"><strong>0,34%</strong></span> and total expense ratio <span class="text-highlight"><strong>0,5%</strong></span>', TEXT_DOMAIN); ?></p>
                     <ul class="list-style-checkmark">
                         <li><?php _e('Most of the money is invested in stocks: bigger expected return and risk', TEXT_DOMAIN); ?></li>
-                        <li><?php _e('Suitable for you if you want <span class=“text-highlight”><strong>best expected return</strong></span> over long term and you are not disturbed by short-term fluctuations of the market', TEXT_DOMAIN); ?></li>
+                        <li><?php _e('Suitable for you if you want <span class="text-highlight"><strong>best expected return</strong></span> over long term and you are not disturbed by short-term fluctuations of the market', TEXT_DOMAIN); ?></li>
                     </ul>
                 </div>
                 <p><hr /></p>
@@ -37,7 +37,7 @@
                     <img class="row-spacing-quarter" src="<?php echo get_template_directory_uri() ?>/img/volakirjad-pie.svg" alt="<?php _e('Tuleva World Bonds Pension Fund', TEXT_DOMAIN); ?>"/>
                 </div>
                 <div class="text-box__content">
-                    <p><?php _e('Low cost: management fee <span class=“text-highlight”><strong>0,34%</strong></span> and total expenses <span class=“text-highlight”><strong>0,5%</strong></span>', TEXT_DOMAIN); ?></p>
+                    <p><?php _e('Low cost: management fee <span class="text-highlight"><strong>0,34%</strong></span> and total expenses <span class="text-highlight"><strong>0,5%</strong></span>', TEXT_DOMAIN); ?></p>
                     <ul class="list-style-checkmark">
                         <li><?php _e('Money is invested into world goverments’ and governmental organisations’ bonds: smaller risk, smaller expected return', TEXT_DOMAIN); ?></li>
                         <li><?php _e('This suits you in case you have just a few years till retirement or in case you are willing to forgo returns in order to avoid losses', TEXT_DOMAIN); ?></li>
