2f7919c21dbe811db022e2054411ca8a0ccdc85f
Erko Risthein <erko@risthein.ee>
2017-07-19 15:28:17 +0300

    Added the heading to the Q&A block

--- src/wp-content/themes/tuleva/templates/components/qa-block.php
+++ src/wp-content/themes/tuleva/templates/components/qa-block.php
@@ -1,3 +1,11 @@
+<div class="bg-alt">
+    <div class="container counter-block">
+        <div class="row row-spacing-half">
+            <h2 class="h1-xs text-center"><?php _e('Answers to frequently asked questions', TEXT_DOMAIN) ?></h2>
+        </div>
+    </div>
+</div>
+
 <div id="<?php the_sub_field('component_id'); ?>" class="<?php echo get_component_classes('container'); ?>">
     <div class="row">
         <div>
