cac197cc487e37debdfe7a311cbf516a6afe8c6e
Erko Risthein <erko@risthein.ee>
2017-07-28 11:09:50 +0300

    Updated signup & login apps, added mixpanel tracking to the membership success page

--- src/wp-content/themes/tuleva/templates/membership-content.php
+++ src/wp-content/themes/tuleva/templates/membership-content.php
@@ -1,23 +1,23 @@
 <div class="page-container">
     <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-    <div class="container row-spacing-half">
-        <div>
-          <div><h2><?php the_content(); ?></h2></div>
-          <div>
-                <h1 class="text-center landing-page-headline"><?php the_field('heading'); ?></h1>
-                <?php //if (isset($_GET['login'])) { ?>
-                    <div>
+        <div class="container row-spacing-half">
+            <div>
+                <div><h2><?php the_content(); ?></h2></div>
+                <div>
+                    <h1 class="text-center landing-page-headline"><?php the_field('heading'); ?></h1>
+                    <div class="col-md-offset-1 col-md-10">
                         <div id="inline-login" class="inline-login"></div>
                         <link href="<?php echo get_template_directory_uri(); ?>/templates/onboarding-client/static/css/login.efe556b6.css" rel="stylesheet">
-                        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/templates/onboarding-client/static/js/login.5133bfa1.js"></script>
-                        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/templates/onboarding-client/static/js/polyfills.8b285ffa.js"></script>
+                        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/templates/onboarding-client/static/js/polyfills.15825c54.js"></script>
+                        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/templates/onboarding-client/static/js/login.b9f93e03.js"></script>
                         <!-- <script type="text/javascript" src="http://localhost:3000/static/js/login.js"></script> -->
-                        <!-- <script type="text/javascript" src="http://localhost:3000/static/js/polyfills.js"></script> -->
+                    <script type="text/javascript">
+                        mixpanel.track("Membership success");
+                    </script>
                     </div>
-                <?php //} ?>
+                </div>
             </div>
         </div>
-    </div>
 
     <?php
 
