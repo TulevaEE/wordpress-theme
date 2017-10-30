ca4c2f2e7752928295cff2b4a6c1ef1b3f44ed69
Sander Värv <sander@pipedrive.com>
2017-10-30 21:49:43 +0200

    Featured articles section

---
+++ src/wp-content/themes/tuleva/templates/components/featured-articles.php
@@ -0,0 +1,36 @@
+<section id="read-blog" class="py-6">
+    <div class="container">
+      <h2 class="mb-5 text-center">Loe meie blogist</h2>
+      <div class="card-deck">
+        <a href="#">
+          <div class="card shadow-sm">
+            <img src="<?php echo get_template_directory_uri() ?>/img/lapskassiga-p-800.jpeg" alt="" class="card-img-top rounded-top">
+            <div class="card-body">
+              <a href="#" class="card-text">
+                Kuidas koguda raha 20-, 30-, 40- ja 50-aastaselt?
+              </a>
+            </div>
+          </div>
+        </a>
+        <a href="#">
+          <div class="card shadow-sm">
+            <img src="<?php echo get_template_directory_uri() ?>/img/tootlusloeb-p-800.jpeg" alt="" class="card-img-top rounded-top">
+            <div class="card-body">
+              <a href="#" class="card-text">
+                Millist tootlust on Tuleva pensionifondis kogudes oodata?
+              </a>
+            </div>
+          </div>
+        </a>
+        <a href="#">
+          <div class="card shadow-sm">
+            <img src="<?php echo get_template_directory_uri() ?>/img/millal-p-800.jpeg" alt="" class="card-img-top rounded-top">
+            <div class="card-body">
+              <a href="#" class="card-text">
+                Kuidas vahetada fondi nii, et kasu saaksid sina, mitte pank või müügiesindaja — 3 head ja 3 halba põhjust</a>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+  </section>
