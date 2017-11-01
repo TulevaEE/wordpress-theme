01901e72df82646a9e4694f0526f100338b1dc04
Sander Värv <sander@pipedrive.com>
2017-11-01 12:33:20 +0200

    Calculator js and testimonial section

---
+++ src/wp-content/themes/tuleva/templates/components/testimonials.php
@@ -0,0 +1,45 @@
+<section id="mentions" class="d-none d-md-block py-6 bg-blue-washed">
+    <div class="container text-center">
+      <h2 class="mb-5">Mida meist räägitakse?</h2>
+      <!-- CAROUSEL -->
+      <div id="carouselControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="col-md-10 col-lg-8 bg-white m-auto p-5 shadow-sm quote">
+              <div class="d-flex flex-column align-items-center">
+                <p class="blockquote text-left mb-5">“Tuleva fond on talupojalikult lihtne ja loogiline. Hea on teada, et raha töötab sinu, mitte kaubandus-keskuses
+                  pensionitooteid müüva müügimehe kasuks.”</p>
+                <div class="d-flex align-items-center">
+                  <img src="<?php echo get_template_directory_uri() ?>/img/henrik-karmo.jpg" width="80" height="80" class="rounded-circle mr-3">
+                  <div class="text-left">Henrik Karmo,
+                    <br>Tuleva asutajaliige</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="col-md-10 col-lg-8 bg-white m-auto p-5 shadow-sm quote">
+              <div class="d-flex flex-column align-items-center">
+                <p class="blockquote text-left mb-5">“Tuleva fond on talupojalikult lihtne ja loogiline. Hea on teada, et raha töötab sinu, mitte kaubandus-keskuses
+                  pensionitooteid müüva müügimehe kasuks.”</p>
+                <div class="d-flex align-items-center">
+                  <img src="<?php echo get_template_directory_uri() ?>/img/daniel-vaarik.jpg" width="80" height="80" class="rounded-circle mr-3">
+                  <div class="text-left">Henrik Karmo,
+                    <br>Tuleva asutajaliige</div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <a class="carousel-control-prev" href="#carouselControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+  </section>
