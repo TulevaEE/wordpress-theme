b62fac72327b85bd2777498eb84e6e210540983b
Sander Värv <sander@pipedrive.com>
2017-11-06 23:50:43 +0200

    Blog beacon url fix

--- src/wp-content/themes/tuleva/templates/footer/blog-beacon.php
+++ src/wp-content/themes/tuleva/templates/footer/blog-beacon.php
@@ -1,4 +1,4 @@
 <div class="blog-beacon">
-    <?php _e('<a class="btn btn-primary btn-block" href="/en/#inline-signup-anchor">Join Tuleva</a>', TEXT_DOMAIN); ?>
+    <?php _e('<a class="btn btn-primary btn-block" href="/en/mutual-company/#inline-signup-anchor">Join Tuleva</a>', TEXT_DOMAIN); ?>
     <?php _e('<a class="btn btn-link btn-block" href="/en/transfer-pension-tuleva/">Transfer pension</a>', TEXT_DOMAIN); ?>
 </div>
