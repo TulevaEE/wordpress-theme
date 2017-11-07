3564765b5d70a2182807dc900c84709d7583301f
Sander Värv <sander@pipedrive.com>
2017-11-07 22:42:58 +0200

    Sticky menu collapse fix

--- src/wp-content/themes/tuleva/templates/header/main-sticky.php
+++ src/wp-content/themes/tuleva/templates/header/main-sticky.php
@@ -4,11 +4,11 @@
             <a class="navbar-brand" href="<?php bloginfo('url') ?>">
                 <img class="brand-logo" alt="Tuleva" src="<?php echo get_template_directory_uri() ?>/img/tuleva-logo.svg">
             </a>
-            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown"
+            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNavDropdownSticky" aria-controls="navbarNavDropdownSticky"
               aria-expanded="false" aria-label="<?php _e('Toggle navigation', TEXT_DOMAIN); ?>">
                 <span class="burger-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <div class="collapse navbar-collapse" id="navbarNavDropdownSticky">
                 <?php get_template_part('templates/header/menu'); ?>
                 <?php language_picker(); ?>
             </div>
