9fcf2a9b712e9a92652fb1780e9da62dafa2a6ce
Sander Värv <sander@pipedrive.com>
2017-11-08 14:52:26 +0200

    Missing div

--- src/wp-content/themes/tuleva/templates/components/fund-choosing.php
+++ src/wp-content/themes/tuleva/templates/components/fund-choosing.php
@@ -4,47 +4,49 @@
             <?php _e('Which Tuleva pension fund to choose?', TEXT_DOMAIN) ?>
         </h2>
         <div class="row">
-            <div class="card-deck">
-                <div class="card shadow-sm">
-                    <div class="card-body text-center pt-5">
-                        <h6 class="mb-4">
-                            <?php _e('If you are less than 55 years old, then the fund suitable to you is', TEXT_DOMAIN); ?>
-                        </h6>
-                        <h4>
-                            <?php _e('Tuleva World Stocks Pension Fund', TEXT_DOMAIN); ?>
-                        </h4>
-                        <img src="<?php echo get_template_directory_uri() ?>/img/aktsiafond-pie.svg?v2" class="img-fluid my-5" alt="<?php _e('Tuleva World Stocks Pension Fund', TEXT_DOMAIN); ?>">
-                        <p class="text-left"><?php _e('The equity fund with lowest costs in Estonia: management fee <span class="text-highlight"><strong>0,34%</strong></span> and total expense ratio <span class="text-highlight"><strong>0,5%</strong></span>', TEXT_DOMAIN); ?></p>
-                        <ul class="text-left list-style-checkmark mb-0">
-                            <li><?php _e('Most of the money is invested in stocks: bigger expected return and risk', TEXT_DOMAIN); ?></li>
-                            <li><?php _e('Suitable for you if you want <span class="text-highlight"><strong>best expected return</strong></span> over long term and you are not disturbed by short-term fluctuations of the market', TEXT_DOMAIN); ?></li>
-                        </ul>
+            <div class="col">
+                <div class="card-deck">
+                    <div class="card shadow-sm">
+                        <div class="card-body text-center pt-5">
+                            <h6 class="mb-4">
+                                <?php _e('If you are less than 55 years old, then the fund suitable to you is', TEXT_DOMAIN); ?>
+                            </h6>
+                            <h4>
+                                <?php _e('Tuleva World Stocks Pension Fund', TEXT_DOMAIN); ?>
+                            </h4>
+                            <img src="<?php echo get_template_directory_uri() ?>/img/aktsiafond-pie.svg?v2" class="img-fluid my-5" alt="<?php _e('Tuleva World Stocks Pension Fund', TEXT_DOMAIN); ?>">
+                            <p class="text-left"><?php _e('The equity fund with lowest costs in Estonia: management fee <span class="text-highlight"><strong>0,34%</strong></span> and total expense ratio <span class="text-highlight"><strong>0,5%</strong></span>', TEXT_DOMAIN); ?></p>
+                            <ul class="text-left list-style-checkmark mb-0">
+                                <li><?php _e('Most of the money is invested in stocks: bigger expected return and risk', TEXT_DOMAIN); ?></li>
+                                <li><?php _e('Suitable for you if you want <span class="text-highlight"><strong>best expected return</strong></span> over long term and you are not disturbed by short-term fluctuations of the market', TEXT_DOMAIN); ?></li>
+                            </ul>
+                        </div>
+                        <div class="bg-white m-4">
+                            <hr class="mb-4">
+                            <?php _e('<a href="/en/tuleva-world-stocks-pension-fund/" class="text-uppercase text-medium d-block mb-4">See fund info</a>', TEXT_DOMAIN); ?>
+                            <?php _e('<a href="/en/transfer-pension-tuleva/" class="btn btn-primary btn-lg btn-block">Select this fund</a>', TEXT_DOMAIN); ?>
+                        </div>
                     </div>
-                    <div class="bg-white m-4">
-                        <hr class="mb-4">
-                        <?php _e('<a href="/en/tuleva-world-stocks-pension-fund/" class="text-uppercase text-medium d-block mb-4">See fund info</a>', TEXT_DOMAIN); ?>
-                        <?php _e('<a href="/en/transfer-pension-tuleva/" class="btn btn-primary btn-lg btn-block">Select this fund</a>', TEXT_DOMAIN); ?>
-                    </div>
-                </div>
-                <div class="card shadow-sm">
-                    <div class="card-body text-center pt-5">
-                        <h6 class="mb-4">
-                            <?php _e('If you are over 55, the fund better suited for you could be', TEXT_DOMAIN); ?>
-                        </h6>
-                        <h4>
-                            <?php _e('Tuleva World Bonds Pension Fund', TEXT_DOMAIN); ?>
-                        </h4>
-                        <img src="<?php echo get_template_directory_uri() ?>/img/volakirjad-pie.svg?v2" class="img-fluid my-5" alt="<?php _e('Tuleva World Bonds Pension Fund', TEXT_DOMAIN); ?>">
-                        <p class="text-left"><?php _e('Low cost: management fee <span class="text-highlight"><strong>0,34%</strong></span> and total expenses <span class="text-highlight"><strong>0,5%</strong></span>', TEXT_DOMAIN); ?></p>
-                        <ul class="text-left list-style-checkmark mb-0">
-                            <li><?php _e('Money is invested into world goverments’ and governmental organisations’ bonds: smaller risk, smaller expected return', TEXT_DOMAIN); ?></li>
-                            <li><?php _e('This suits you in case you have just a few years till retirement or in case you are willing to forgo returns in order to avoid losses', TEXT_DOMAIN); ?></li>
-                        </ul>
-                    </div>
-                    <div class="bg-white m-4">
-                        <hr class="mb-4">
-                        <?php _e('<a href="/en/tuleva-world-bonds-pension-fund/" class="text-uppercase text-medium d-block mb-4">See fund info</a>', TEXT_DOMAIN); ?>
-                        <?php _e('<a href="/en/transfer-pension-tuleva/" class="btn btn-primary btn-lg btn-block">Select this fund</a>', TEXT_DOMAIN); ?>
+                    <div class="card shadow-sm">
+                        <div class="card-body text-center pt-5">
+                            <h6 class="mb-4">
+                                <?php _e('If you are over 55, the fund better suited for you could be', TEXT_DOMAIN); ?>
+                            </h6>
+                            <h4>
+                                <?php _e('Tuleva World Bonds Pension Fund', TEXT_DOMAIN); ?>
+                            </h4>
+                            <img src="<?php echo get_template_directory_uri() ?>/img/volakirjad-pie.svg?v2" class="img-fluid my-5" alt="<?php _e('Tuleva World Bonds Pension Fund', TEXT_DOMAIN); ?>">
+                            <p class="text-left"><?php _e('Low cost: management fee <span class="text-highlight"><strong>0,34%</strong></span> and total expenses <span class="text-highlight"><strong>0,5%</strong></span>', TEXT_DOMAIN); ?></p>
+                            <ul class="text-left list-style-checkmark mb-0">
+                                <li><?php _e('Money is invested into world goverments’ and governmental organisations’ bonds: smaller risk, smaller expected return', TEXT_DOMAIN); ?></li>
+                                <li><?php _e('This suits you in case you have just a few years till retirement or in case you are willing to forgo returns in order to avoid losses', TEXT_DOMAIN); ?></li>
+                            </ul>
+                        </div>
+                        <div class="bg-white m-4">
+                            <hr class="mb-4">
+                            <?php _e('<a href="/en/tuleva-world-bonds-pension-fund/" class="text-uppercase text-medium d-block mb-4">See fund info</a>', TEXT_DOMAIN); ?>
+                            <?php _e('<a href="/en/transfer-pension-tuleva/" class="btn btn-primary btn-lg btn-block">Select this fund</a>', TEXT_DOMAIN); ?>
+                        </div>
                     </div>
                 </div>
             </div>
