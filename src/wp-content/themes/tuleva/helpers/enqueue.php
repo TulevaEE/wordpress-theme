d47e93bdf089fc3fc2df82371eca13d25b2b685f
Sander Värv <sander@pipedrive.com>
2017-11-08 13:18:24 +0200

    Moz input spinner removal

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -27,7 +27,7 @@ class ThemeEnqueue {
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '07112017_2',
+                'ver' => '08112017',
                 'media' => 'all',
                 'enqueue' => true
             ],
