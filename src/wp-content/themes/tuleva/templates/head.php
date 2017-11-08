fdae9d65320b9cec5da553ef031c8bb94d256621
Sander Värv <sander@pipedrive.com>
2017-11-08 22:05:05 +0200

    Added better meta description

--- src/wp-content/themes/tuleva/templates/head.php
+++ src/wp-content/themes/tuleva/templates/head.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="description" content="<?php bloginfo('name'); ?>">
+    <meta name="description" content="<?php bloginfo('description'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://cdn.optimizely.com/js/8423232854.js"></script>
     <!-- start Mixpanel -->
