849ce6a5875d96fc70ee7d981d51ebe77535662d
Sander Värv <sander@pipedrive.com>
2017-11-15 08:22:10 +0200

    Contacts page update

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -27,7 +27,7 @@ class ThemeEnqueue {
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '13112017',
+                'ver' => '15112017',
                 'media' => 'all',
                 'enqueue' => true
             ],
--- src/wp-content/themes/tuleva/templates/contact-content.php
+++ src/wp-content/themes/tuleva/templates/contact-content.php
@@ -1,43 +1,68 @@
 <div class="page-container">
     <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-        <div class="container">
-            <div class="row row-spacing-bottom">
-                <div class="col-md-12">
+    <div class="bg-blue-washed">
+        <div class="container py-6">
+            <div class="row">
+                <div class="col">
                     <?php if (get_field('heading')) { ?>
-                        <h1 class="page-title"><?php the_field('heading'); ?></h1>
+                    <h1 class="text-center">
+                        <?php the_field('heading'); ?>
+                    </h1>
                     <?php } ?>
                     <?php if (get_field('subheading')) { ?>
-                        <h2 class="text-center text-normal"><?php the_field('subheading'); ?></h2>
+                    <h4 class="text-center text-normal">
+                        <?php the_field('subheading'); ?>
+                    </h4>
                     <?php } ?>
-                </div>
-            </div>
-            <div class="row row-spacing">
-                <div class="col-md-12">
-                    <div class="row contacts-block">
-                        <?php if (have_rows('contacts')) {
-                            while (have_rows('contacts')) { the_row(); ?>
-                                <div class="col-md-4 contacts-block__item">
-                                    <?php if(get_sub_field('image')) { ?>
-                                        <img class="contacts-block__image" src="<?php the_sub_field('image'); ?>" alt="<?php the_sub_field('name'); ?>">
-                                    <?php } ?>
-
-                                    <h2 class="contacts-block__title"><?php the_sub_field('name'); ?></h2>
-
-                                    <?php if(get_sub_field('email')) { ?>
-                                        <div class="contacts-block__row">
-                                            <a href="mailto:<?php the_sub_field('email'); ?>"><?php the_sub_field('email'); ?></a>
-                                        </div>
-                                    <?php } ?>
-
-                                    <?php if(get_sub_field('phone')) { ?>
-                                        <div class="contacts-block__row"><?php the_sub_field('phone'); ?></div>
-                                    <?php } ?>
-                                </div>
-                        <?php  }
-                        } ?>
-                    </div>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="container pb-6">
+        <?php if( have_rows('contacts_block') ) while ( have_rows('contacts_block') ) : the_row(); ?>
+        <div class="row pt-6">
+            <div class="col">
+                <h2 class="text-center">
+                    <?php the_sub_field('block_title'); ?>
+                </h2>
+                <div class="row contacts-block">
+                    <?php if (have_rows('contacts')) {
+                            while (have_rows('contacts')) { the_row(); ?>
+                    <div class="contacts-block__item">
+                        <?php if(get_sub_field('image')) { ?>
+                        <img class="contacts-block__image" src="<?php the_sub_field('image'); ?>" alt="<?php the_sub_field('name'); ?>">
+                        <?php } ?>
+
+                        <h4 class="contacts-block__title">
+                            <?php the_sub_field('name'); ?>
+                        </h4>
+
+                        <?php if(get_sub_field('role')) { ?>
+                        <div class="contacts-block__row">
+                            <?php the_sub_field('role'); ?>
+                        </div>
+                        <?php } ?>
+
+                        <?php if(get_sub_field('email')) { ?>
+                        <div class="contacts-block__row">
+                            <a href="mailto:<?php the_sub_field('email'); ?>">
+                                <?php the_sub_field('email'); ?>
+                            </a>
+                        </div>
+                        <?php } ?>
+
+                        <?php if(get_sub_field('phone')) { ?>
+                        <div class="contacts-block__row">
+                            <?php the_sub_field('phone'); ?>
+                        </div>
+                        <?php } ?>
+                    </div>
+                    <?php  }
+                        } ?>
+                </div>
+            </div>
+        </div>
+        <?php endwhile; ?>
+    </div>
     <?php endwhile; ?>
 </div>
