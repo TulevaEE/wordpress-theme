a674537e575109b626e048e31b696e04b3e30fff
Sander Värv <sander@varv.eu>
2017-12-29 14:00:40 +0200

    Blog beacon CTA enque

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -27,7 +27,7 @@ class ThemeEnqueue {
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '21112017',
+                'ver' => '29122017',
                 'media' => 'all',
                 'enqueue' => true
             ],
