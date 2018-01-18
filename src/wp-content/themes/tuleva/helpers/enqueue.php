facb5fcbc3421e71b16d2eb0212a557c465b32ad
Sander Värv <sander@varv.eu>
2018-01-18 14:11:06 +0200

    Another enqueue update

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -27,7 +27,7 @@ class ThemeEnqueue {
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '29122017',
+                'ver' => '18012018',
                 'media' => 'all',
                 'enqueue' => true
             ],
