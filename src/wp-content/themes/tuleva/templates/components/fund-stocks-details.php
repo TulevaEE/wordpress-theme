da018ee6abe897cb73691a7da95ef0ff83818046
Sander Värv <sander@varv.eu>
2018-02-08 09:31:09 +0200

    Report updates

--- src/wp-content/themes/tuleva/templates/components/fund-stocks-details.php
+++ src/wp-content/themes/tuleva/templates/components/fund-stocks-details.php
@@ -75,7 +75,7 @@
                             <li>
                                 Investeeringute aruanded
                                 <br>
-                                <a href="https://tuleva.ee/wp-content/uploads/2018/01/TULAPF-investeeringute-aruanne-311217.pdf" target="_blank">12.17</a>
+                                <a href="https://tuleva.ee/wp-content/uploads/2018/02/TULAPF-investeeringute-aruanne-310118.docx.pdf" target="_blank">12.17</a>
                                 <br>
                                 <a href="http://www.pensionikeskus.ee/ii-sammas/fondid/kohustuslikud-pensionifondid/77/" target="_blank">Eelnevad aruanded</a>
                             </li>
