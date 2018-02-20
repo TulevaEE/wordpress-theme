033505df68a642b66e7c14c8ceb451e0666f80e6
Sander Värv <sander@varv.eu>
2018-02-20 11:07:28 +0200

    Content updates

--- src/wp-content/themes/tuleva/templates/difference-content.php
+++ src/wp-content/themes/tuleva/templates/difference-content.php
@@ -3,9 +3,8 @@
         <div class="container">
             <div class="row py-5">
                 <div class="col-lg-8 mx-auto text-center">
-                    <p class="lead text-navy">Tuleva pensionifondid on pankade omadega üsna sarnased.</br>
-                    Välja arvatud peamine.</p>
-                    <h2><span class="text-highlight">Hoiame tasud hästi madalal</span></h2>
+                    <h2><span class="text-highlight">Tulevas hoiame tasud hästi madalal</span></h2>
+                    <p class="lead text-navy mb-0">See ongi peamine. Muus osas oleme pankade pensionifondidega üsna sarnased.</p>
                 </div>
             </div>
         </div>
@@ -24,16 +23,17 @@
                         </button>
                         <p>Aga andmenohikud on leinud, et mida madalamad kulud, seda paremad eeldused, et tootlus tuleb hea.</p>
                     </div>
-                    <div id="collapseOne" class="collapse my-5" aria-labelledby="headingOne" data-parent="#accordion">
+                    <div id="collapseOne" class="collapse mt-2 mb-5" aria-labelledby="headingOne" data-parent="#accordion">
                         <div>
                             <p>Pangad teevad näo, et kulud pole tegelikult tähtsad. Warren Buffett pole nõus. Analüütikud pole ka nõus. Nad on aastate viisi andmeid läbi töötanud ja leidnud üht: <a href="http://news.morningstar.com/articlenet/article.aspx?id=752485" target="_blank">madalad kulud on kõige tõsisem fondi tootluse ennustaja</a>.</p>
 
                             <p>Teisisõnu: <span class="text-highlight text-bold">mida madalamad on pensionifondi kulud, seda suurem tõenäosus, et sinu pensionivara kasvab rohkem.</span></p>
 
+                            <p>Asi on lihtsalt selles, et maailmamajanduse käekäik pole meie kontrolli all. Väärtpaberiturgude tõuse ja torme ette ennustada on üsna võimatu. Aga kulud on selge suurus: mida rohkem läheb tasudeks, seda vähem jääb sulle tulust, mida sinu raha turgudel teenib.</p>
+
                             <img src="<?php echo get_template_directory_uri() ?>/img/fig-principle-success-probability.png" class="img-fluid" alt="Odava tasuga fondid saavad parima tulemuse" />
                             <cite class="d-block small text-secondary mt-3 mb-5">Allikas: Maailma suurima fondide analüüsi firma Morningstar <a href="http://news.morningstar.com/articlenet/article.aspx?id=752485" target="_blank">analüüs</a> USA investeerimisfondide 2010-2015 tulemuste põhjal.</cite>
 
-                            <p>Asi on lihtsalt selles, et maailmamajanduse käekäik pole meie kontrolli all. Väärtpaberiturgude tõuse ja torme ette ennustada on üsna võimatu. Aga kulud on selge suurus: mida rohkem läheb tasudeks, seda vähem jääb sulle tulust, mida sinu raha turgudel teenib.</p>
                         </div>
                     </div>
                 </div>
@@ -44,17 +44,17 @@
                             aria-controls="collapseTwo">Meie usume nohikuid</button>
                         <p>Me usume, et pangad usuvad tegelikult ka. Lihtsalt nemad peavad omanikele kasumit teenima.</p>
                     </div>
-                    <div id="collapseTwo" class="collapse my-5" aria-labelledby="headingTwo" data-parent="#accordion">
+                    <div id="collapseTwo" class="collapse mt-2 mb-5" aria-labelledby="headingTwo" data-parent="#accordion">
 
 
                         <a>Pole kokkusattumus, et meie pankade pensionifondid paistavad silma nii kõrgete tasude kui ka kehvade tulemustega. Näiteks 2016. aastal võtsid pankade fondivalitsejad igast eurost, mis Eesti inimeste raha turgudel teenis, <a href="https://tuleva.ee/analuusid/fondivalitsejate-aastaaruanded-2016/" target="_blank">40 senti endale</a>.</p>
 
-                        <img src="<?php echo get_template_directory_uri() ?>/img/fig-principle-oecd-comparison.png" class="img-fluid" alt="Eestis on pensionifondide tasud ühed OECD kõrgemad ja tootlus kõige madalam" />
-                        <cite class="d-block small text-secondary mt-3 mb-5">Allikas: Postimees, <a href="https://majandus24.postimees.ee/4292311/eesti-pensionifondid-endiselt-oecd-kehvimad" target="_blank">Eesti pensionifondid endiselt OECD kehvimad</a>.</cite>
-
                         <p>Pankade omanikele kõrged tasud meeldivad. Mida rohkem sa maksad, seda suurem on fondivalitseja kasum.</p>
 
                         <p><span class="text-highlight text-bold">Tuleval on teine eesmärk</span>. Meie, pensionikogujad, tegime ise oma fondid, et saada igast täna säästetud eurost pensionile jõudmise ajaks rohkem kasu.</p>
+
+                        <img src="<?php echo get_template_directory_uri() ?>/img/fig-principle-oecd-comparison.png" class="img-fluid" alt="Eestis on pensionifondide tasud ühed OECD kõrgemad ja tootlus kõige madalam" />
+                        <cite class="d-block small text-secondary mt-3 mb-5">Allikas: Postimees, <a href="https://majandus24.postimees.ee/4292311/eesti-pensionifondid-endiselt-oecd-kehvimad" target="_blank">Eesti pensionifondid endiselt OECD kehvimad</a>.</cite>
                     </div>
                 </div>
                 <hr />
@@ -66,7 +66,7 @@
                         </button>
                         <p>Enamus inimeste rahast võetud valitsemistasudest kulubki pankade fondivalitsejatel just nendele asjadele.</p>
                     </div>
-                    <div id="collapseThree" class="collapse my-5" aria-labelledby="headingThree" data-parent="#accordion">
+                    <div id="collapseThree" class="collapse mt-2 mb-5" aria-labelledby="headingThree" data-parent="#accordion">
 
                         <p>See võib sind üllatada, aga pankade fondivalitsejate võetud tasudest <span class="text-highlight text-bold">vaid umbes veerand läheb pensionikogujate vara eest hoolitsemisele</span>.</p>
 
@@ -84,7 +84,7 @@
                         </button>
                         <p>Meie fondid on juba täna mitu korda soodsama tasuga.</p>
                     </div>
-                    <div id="collapseFour" class="collapse my-5" aria-labelledby="headingFour" data-parent="#accordion">
+                    <div id="collapseFour" class="collapse mt-2 mb-5" aria-labelledby="headingFour" data-parent="#accordion">
                         <p>Meie ühistes fondides ei kulu sinu raha</p>
                         <ul class="list-style-cross mb-5">
                             <li>Panga kasumiks</li>
@@ -93,18 +93,17 @@
                             <li>ennustamatu ennustamiseks</li>
                         </ul>
 
-                        <img src="<?php echo get_template_directory_uri() ?>/img/fig-principle-fee-comparison.png" class="img-fluid" alt="Kui palju maksad iga 10 000€ pealt keskmiselt teenustasudeks?" />
-                        <cite class="d-block small text-secondary mt-3 mb-5">Allikas: Pensionikeskus, Rahandusministeerium. <a href="https://docs.google.com/spreadsheets/d/1fl45EhacB_HLb1yPPsi-jfkwA4Vt8KEi_e-aQ8N3uUo/edit#gid=0" target="_blank">Eesti pensionifondide jooksvate kulude tabel</a></cite>
-
                         <p><span class="text-highlight text-bold">Kui kasvame, saame tasud veel madalamaks.</span></p>
 
+                        <img src="<?php echo get_template_directory_uri() ?>/img/fig-principle-fee-comparison.png" class="img-fluid" alt="Kui palju maksad iga 10 000€ pealt keskmiselt teenustasudeks?" />
+                        <cite class="d-block small text-secondary mt-3 mb-5">Allikas: Pensionikeskus, Rahandusministeerium. <a href="https://docs.google.com/spreadsheets/d/1fl45EhacB_HLb1yPPsi-jfkwA4Vt8KEi_e-aQ8N3uUo/edit#gid=0" target="_blank">Eesti pensionifondide jooksvate kulude tabel</a></cite>
                     </div>
                 </div>
                 <hr />
             </div>
         </div>
 
-        <div class="row my-6">
+        <div class="row mt-6">
             <div class="col-md-10 col-lg-8 mx-auto">
                 <h3 class="text-center mb-5">Tasud on palju suurema mõjuga kui sa arvad.</h3>
                 <p>Kui palju need tillukesed numbrid ikka tulemust mõjutada saavad, küsid sa. Vaatame lähemalt.</p>
@@ -115,20 +114,18 @@
 
                 <p>Martinile koguneb pensioniks kokku 185 tuhat eurot. Sirli kogub sama ajaga 215 tuhat eurot. Pealtnäha <span class="text-highlight text-bold">pisike erinevus tasudes läks Martinile maksma ligi 30 000 eurot!</span></p>
 
-                <img src="<?php echo get_template_directory_uri() ?>/img/fig-martinsirli.png" class="img-fluid text-center" alt="Teenustasude kulu" />
-                <cite class="d-block small text-secondary mt-3 mb-5">Eeldame, et Sirli ja Martini palk kasvab 3% aastas ning mõlema fondi tootlus enne teenustasu mahaarvamist on 5% aastas. Pea meeles, et tootlus ei ole ei Tuleva ega üheski teises Eesti pensionifondis garanteeritud ja võib olla nii positiivne kui negatiivne.</cite>
+                <img src="<?php echo get_template_directory_uri() ?>/img/fig-martinsirli.png" class="img-fluid  text-center" alt="Teenustasude kulu" />
 
-                <a class="btn btn-link btn-block text-center h5 my-5" href="#">Loe lähemalt meie blogist</a>
-                <hr />
+                <a class="btn btn-link btn-block text-center  text-bold mt-4 mb-6" href="#">Loe lähemalt, ja vaata, milliste eeldustega need näited on tehtud.</a>
             </div>
         </div>
 
         <div class="row">
             <div class="col-md-10 col-lg-8 mx-auto">
-                <div class="text-center">
-                    <h3 class="mb-4">5 soovitust pensionifondi valikuks</h3>
-                    <a class="btn btn-primary btn-lg d-block d-md-inline-block mb-4 mb-md-0 mr-md-4" href="https://tuleva.ee/vali-pensionifond-targalt/">Olen noorem kui 55</a>
-                    <a class="btn btn-primary btn-lg d-block d-md-inline-block" href="https://tuleva.ee/valmistu-pensioniks-targalt/">Olen vanem kui 55</a>
+                <div class="text-center emphasis-box py-5">
+                    <h4 class="mb-4">5 soovitust — kuidas pensionifondi valida?</h4>
+                    <a class="btn btn-primary btn-lg d-block d-md-inline-block mb-4 mb-md-0 mr-md-4" href="https://tuleva.ee/vali-pensionifond-targalt/">Vanus 18-55</a>
+                    <a class="btn btn-primary btn-lg d-block d-md-inline-block" href="https://tuleva.ee/valmistu-pensioniks-targalt/">Vanus üle 55</a>
                 </div>
             </div>
         </div>
