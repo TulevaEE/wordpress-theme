337f752424f43cb9804686c96bd7161471102bab
Sander Värv <sander@varv.eu>
2018-02-27 17:13:32 +0200

    Bg image swap

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -27,7 +27,7 @@ class ThemeEnqueue {
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '26022018',
+                'ver' => '27022018',
                 'media' => 'all',
                 'enqueue' => true
             ],
