76f1d4b7d440cc0cca3c3119c27662e04180fa76
Sander Värv <sander@varv.eu>
2018-02-27 16:06:25 +0200

    CTA section copy update

--- src/wp-content/themes/tuleva/templates/components/cta-change.php
+++ src/wp-content/themes/tuleva/templates/components/cta-change.php
@@ -2,7 +2,7 @@
     <div class="container">
       <div class="row text-center">
         <div class="d-flex flex-column flex-md-row w-100 justify-content-center align-items-center">
-          <h5 class="mb-md-0 mr-md-3"><?php _e('Switching funds is free<span class="d-none d-lg-inline-block"> and takes only five minutes</span>.', TEXT_DOMAIN); ?></h5>
+          <h5 class="mb-md-0 mr-md-3"><?php _e('Switching funds is FREE and takes only five minutes.', TEXT_DOMAIN); ?></h5>
           <?php _e('<a href="/en/transfer-pension-tuleva/" class="btn btn-primary btn-lg">Switch to Tuleva</a>', TEXT_DOMAIN); ?>
         </div>
       </div>
