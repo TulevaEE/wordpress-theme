0c7ab0152db5a709ec5282d52e89abfa2bc1b4fb
Carl Rannaberg <carl@pipedrive.com>
2018-06-15 14:18:01 +0300

    Style version bump.

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -27,7 +27,7 @@ class ThemeEnqueue {
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '17042018',
+                'ver' => '15062018',
                 'media' => 'all',
                 'enqueue' => true
             ],
