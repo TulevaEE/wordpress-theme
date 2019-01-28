824c5953efe2a55a42bb683a49066678907e8236
Sander Värv <sander@varv.eu>
2019-01-28 18:01:04 +0200

    Enque JS version bump

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -110,7 +110,7 @@ class ThemeEnqueue {
                 'handle' => 'calculator',
                 'src' => $js_path . 'calculator.js',
                 'deps' => 'jquery',
-                'ver' => '15062018',
+                'ver' => '28012019',
                 'js_in_header' => false,
                 'enqueue' => true
             ]
