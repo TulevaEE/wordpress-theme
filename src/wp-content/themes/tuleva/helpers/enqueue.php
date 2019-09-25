e5d2fd78a410c2f27aeb453d061f1c42bb78a765
Carl Rannaberg <carl@pipedrive.com>
2019-09-25 23:10:13 +0300

    Updated main.js cachebreaker.

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -99,7 +99,7 @@ class ThemeEnqueue {
                 'handle' => 'main-script',
                 'src' => $js_path . 'main.js',
                 'deps' => 'jquery',
-                'ver' => '17042018',
+                'ver' => '25092019',
                 'js_in_header' => false,
                 'enqueue' => true
             ]
