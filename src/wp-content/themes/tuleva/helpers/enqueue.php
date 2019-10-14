c46e962c36e9bc04040a7b9d138146be4645ac7a
Carl Rannaberg <carl@pipedrive.com>
2019-10-14 21:21:29 +0300

    Change blog post list spacing.

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -27,7 +27,7 @@ class ThemeEnqueue {
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '19062018',
+                'ver' => '14102019',
                 'media' => 'all',
                 'enqueue' => true
             ],
