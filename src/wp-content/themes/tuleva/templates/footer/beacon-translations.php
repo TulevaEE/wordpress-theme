bc44f5737ee10a927e522f24878bf080051a3150
Carl Rannaberg <carlrannaberg@gmail.com>
2020-01-29 16:14:23 +0200

    Hide Help Scount default button.

--- src/wp-content/themes/tuleva/templates/footer/beacon-translations.php
+++ src/wp-content/themes/tuleva/templates/footer/beacon-translations.php
@@ -1,17 +1,15 @@
 <script>
     window.Beacon('config', {
-        translation: {
-          contactLabel: '<?php _e("Send a Message", TEXT_DOMAIN); ?>',
-          attachFileLabel: '<?php _e("Attach a file", TEXT_DOMAIN); ?>',
-          attachFileError: '<?php _e("The maximum file size is 10mb", TEXT_DOMAIN); ?>',
-          fileExtensionError: '<?php _e("The file format you uploaded is not allowed.", TEXT_DOMAIN); ?>',
-          emailLabel: '<?php _e("Email address", TEXT_DOMAIN); ?>',
-          emailError: '<?php _e("Please enter a valid email address", TEXT_DOMAIN); ?>',
-          messageLabel: '<?php _e("How can we help you?", TEXT_DOMAIN); ?>',
-          messageError: '<?php _e("Please enter a message", TEXT_DOMAIN); ?>',
-          contactSuccessLabel: '<?php _e("Message sent!", TEXT_DOMAIN); ?>',
-          contactSuccessDescription: '<?php _e("Thanks for reaching out! Someone from our team will get back to you soon.", TEXT_DOMAIN); ?>',
-          sendLabel: '<?php _e("Send", TEXT_DOMAIN); ?>'
-        }
+        contactLabel: '<?php _e("Send a Message", TEXT_DOMAIN); ?>',
+        attachFileLabel: '<?php _e("Attach a file", TEXT_DOMAIN); ?>',
+        attachFileError: '<?php _e("The maximum file size is 10mb", TEXT_DOMAIN); ?>',
+        fileExtensionError: '<?php _e("The file format you uploaded is not allowed.", TEXT_DOMAIN); ?>',
+        emailLabel: '<?php _e("Email address", TEXT_DOMAIN); ?>',
+        emailError: '<?php _e("Please enter a valid email address", TEXT_DOMAIN); ?>',
+        messageLabel: '<?php _e("How can we help you?", TEXT_DOMAIN); ?>',
+        messageError: '<?php _e("Please enter a message", TEXT_DOMAIN); ?>',
+        contactSuccessLabel: '<?php _e("Message sent!", TEXT_DOMAIN); ?>',
+        contactSuccessDescription: '<?php _e("Thanks for reaching out! Someone from our team will get back to you soon.", TEXT_DOMAIN); ?>',
+        sendLabel: '<?php _e("Send", TEXT_DOMAIN); ?>'
     });
 </script>
