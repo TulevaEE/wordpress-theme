a5f76f2a1178429853a350417f6bc6d25f711030
Tõnu Pekk <tonu.pekk@tuleva.ee>
2020-02-11 13:43:07 +0200

    update fund names eng

--- src/wp-content/themes/tuleva/templates/components/front-header.php
+++ src/wp-content/themes/tuleva/templates/components/front-header.php
@@ -70,7 +70,7 @@
                                             SEB Energiline Pensionifond
                                         </option>
                                         <option value="SIK75">
-                                            SEB Energiline Pensionifond Indeks
+                                            SEB Pensionifond Indeks 100
                                         </option>
                                         <option value="SEK00">
                                             SEB Konservatiivne Pensionifond
@@ -82,19 +82,19 @@
                                             SEB Progressiivne Pensionifond
                                         </option>
                                         <option value="SWK00">
-                                            Swedbank Pensionifond K1 (Konservatiivne strateegia)
+                                            Swedbank Pensionifond K10
                                         </option>
                                         <option value="SWK25">
-                                            Swedbank Pensionifond K2 (Tasakaalustatud strateegia)
+                                            Swedbank Pensionifond K30
                                         </option>
                                         <option value="SWK50">
-                                            Swedbank Pensionifond K3 (Kasvustrateegia)
+                                            Swedbank Pensionifond K60
                                         </option>
                                         <option value="SWK75">
-                                            Swedbank Pensionifond K4 (Aktsiastrateegia)
+                                            Swedbank Pensionifond K100
                                         </option>
                                         <option value="SWK99">
-                                            Swedbank Pensionifond K90-99 (Elutsükli strateegia)
+                                            Swedbank Pensionifond K1990-1999 indeks
                                         </option>
                                     </select>
                                     <div class="calculator__comparison-result">
