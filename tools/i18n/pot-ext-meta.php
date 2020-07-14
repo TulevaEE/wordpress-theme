072c5f2e30042e4bf05aef01d87a74900b329ccd
Aleksandra Isabel Pillmann <isabelpillmann@gmail.com>
2020-07-14 12:06:38 +0300

    Fix translations on fund pages, change pot query files

--- tools/i18n/pot-ext-meta.php
+++ tools/i18n/pot-ext-meta.php
@@ -7,7 +7,7 @@
  * @subpackage tools
  */
 
-$pomo = dirname( dirname( dirname( __FILE__ ) ) ) . '/src/wp-includes/pomo';
+$pomo = dirname( dirname( dirname( __FILE__ ) ) ) . '/wordpress/wp-includes/pomo';
 require_once "$pomo/po.php";
 require_once dirname( __FILE__ ) . '/makepot.php';
 
