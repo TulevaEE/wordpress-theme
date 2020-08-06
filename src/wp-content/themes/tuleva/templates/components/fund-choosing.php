1f59c4c909889b52e34f429e750086007ef8b8b8
Aleksandra Isabel Pillmann <isabelpillmann@gmail.com>
2020-08-06 15:50:13 +0300

    Change percentage on fund choosing page

--- src/wp-content/themes/tuleva/templates/components/fund-choosing.php
+++ src/wp-content/themes/tuleva/templates/components/fund-choosing.php
@@ -21,7 +21,7 @@
                                 <?php _e('<a href="/en/online-bank-instructions/" class="btn btn-primary btn-lg btn-block" id="fund-choose-stocks">Select this fund</a>', TEXT_DOMAIN); ?>
                             </div>
                             <hr class="mb-4">
-                            <p class="text-left"><?php _e('The equity fund with lowest costs in Estonia*: management fee <span class="text-highlight"><strong>0,34%</strong></span> and total expense ratio <span class="text-highlight"><strong>0,46%</strong></span>', TEXT_DOMAIN); ?></p>
+                            <p class="text-left"><?php _e('The equity fund with lowest costs in Estonia*: management fee <span class="text-highlight"><strong>0,34%</strong></span> and total expense ratio <span class="text-highlight"><strong>0,45%</strong></span>', TEXT_DOMAIN); ?></p>
                             <ul class="text-left list-style-checkmark mb-0">
                                 <li><?php _e('Most of the money is invested in stocks: bigger expected return and risk', TEXT_DOMAIN); ?></li>
                                 <li><?php _e('Suitable for you if you want <span class="text-highlight"><strong>best expected return</strong></span> over long term and you are not disturbed by short-term fluctuations of the market', TEXT_DOMAIN); ?></li>
@@ -42,7 +42,7 @@
                                 <?php _e('<a href="/en/online-bank-instructions/" class="btn btn-primary btn-lg btn-block" id="fund-choose-bonds">Select this fund</a>', TEXT_DOMAIN); ?>
                             </div>
                             <hr class="mb-4">
-                            <p class="text-left"><?php _e('Low cost: management fee <span class="text-highlight"><strong>0,34%</strong></span> and total expenses <span class="text-highlight"><strong>0,49%</strong></span>', TEXT_DOMAIN); ?></p>
+                            <p class="text-left"><?php _e('Low cost: management fee <span class="text-highlight"><strong>0,34%</strong></span> and total expenses <span class="text-highlight"><strong>0,47%</strong></span>', TEXT_DOMAIN); ?></p>
                             <ul class="text-left list-style-checkmark mb-0">
                                 <li><?php _e('Money is invested into world goverments’ and governmental organisations’ bonds: smaller risk, smaller expected return', TEXT_DOMAIN); ?></li>
                                 <li><?php _e('This suits you in case you have just a few years till retirement or in case you are willing to forgo returns in order to avoid losses', TEXT_DOMAIN); ?></li>
