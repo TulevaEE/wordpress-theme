b84b15f9a6879f54aa42437a5e048f347e00178c
Carl Rannaberg <carlrannaberg@gmail.com>
2021-01-25 16:40:54 +0200

    Fix pensionikeskus links.

--- src/wp-content/themes/tuleva/templates/components/fund-stocks-details.php
+++ src/wp-content/themes/tuleva/templates/components/fund-stocks-details.php
@@ -81,7 +81,7 @@
                                 <br>
                                 <a href="<?php echo get_site_url(); ?>/wp-content/uploads/2021/01/TULAPF-investeeringute-aruanne-311220-.pdf" target="_blank">12.20</a>
                                 <br>
-                                <a href="http://www.pensionikeskus.ee/ii-sammas/fondid/kohustuslikud-pensionifondid/fid/77/" target="_blank"><?php _e('Previous reports', TEXT_DOMAIN) ?></a>
+                                <a href="https://www.pensionikeskus.ee/ii-sammas/kohustuslikud-pensionifondid/fid/77/" target="_blank"><?php _e('Previous reports', TEXT_DOMAIN) ?></a>
                             </li>
                             <li>
                                 <a href="<?php echo get_site_url(); ?>/aruanded/ "><?php _e('Financial reports of fund and fund manager (in Estonian)', TEXT_DOMAIN) ?></a>
