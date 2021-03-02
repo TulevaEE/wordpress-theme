63f8ef8ff4de100a7b8866e92377b4db8f484cb7
Carl Rannaberg <carlrannaberg@gmail.com>
2021-03-02 17:29:21 +0200

    Change log in link style.

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -27,7 +27,7 @@ class ThemeEnqueue {
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '20201109',
+                'ver' => '20210302',
                 'media' => 'all',
                 'enqueue' => true
             ],
