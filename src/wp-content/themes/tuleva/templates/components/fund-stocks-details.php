e04338abcac0810cf9fcd2d8f5433367f0e04218
Carl Rannaberg <carlrannaberg@gmail.com>
2021-03-31 21:00:27 +0300

    Update documents.

--- src/wp-content/themes/tuleva/templates/components/fund-stocks-details.php
+++ src/wp-content/themes/tuleva/templates/components/fund-stocks-details.php
@@ -61,19 +61,16 @@
                         <h5 class="mb-4"><?php _e('Documents', TEXT_DOMAIN) ?></h5>
                         <ul class="list-style-arrow mb-5">
                             <li>
-                                <a href="<?php echo get_site_url(); ?>/wp-content/uploads/2021/02/Prospekt.-TUK75-ja-TUK00.-kehtiv-19.02.2021.pdf" target="_blank"><?php _e('Prospectus', TEXT_DOMAIN) ?></a><?php _e(' and ', TEXT_DOMAIN) ?><a href="<?php echo get_site_url(); ?>/wp-content/uploads/2021/03/Tuleva-Maailma-Aktsiate-Pensionifondi-tingimused-05.03.2021.pdf" target="_blank"><?php _e('Terms and conditions', TEXT_DOMAIN) ?></a><?php _e(' (in Estonian)', TEXT_DOMAIN) ?>
+                                <a href="<?php echo get_site_url(); ?>/wp-content/uploads/2021/03/Prospekt.-TUK75-ja-TUK00.-kehtiv-01.04.2021.pdf" target="_blank"><?php _e('Prospectus', TEXT_DOMAIN) ?></a><?php _e(' and ', TEXT_DOMAIN) ?><a href="<?php echo get_site_url(); ?>/wp-content/uploads/2021/03/Tuleva-Maailma-Aktsiate-Pensionifondi-tingimused-05.03.20211.pdf" target="_blank"><?php _e('Terms and conditions', TEXT_DOMAIN) ?></a><?php _e(' (in Estonian)', TEXT_DOMAIN) ?>
                             </li>
                             <li>
                                 <a href="<?php echo get_site_url(); ?>/wp-content/uploads/2021/03/Tuleva-pensionifondide-mudelportfellid-18.02.2021.pdf" target="_blank"><?php _e('Model portfolio (in Estonian)', TEXT_DOMAIN) ?></a>
                             </li>
                             <li>
-                                <a href="<?php echo get_site_url(); ?>/wp-content/uploads/2021/02/Pohiteave.-TUK75_19.02.2021.pdf" target="_blank"><?php _e('Key Investor Information (in Estonian)', TEXT_DOMAIN) ?></a>
+                                <a href="<?php echo get_site_url(); ?>/wp-content/uploads/2021/03/Pohiteave.-TUK75_01.04.2021.pdf" target="_blank"><?php _e('Key Investor Information (in Estonian)', TEXT_DOMAIN) ?></a>
                             </li>
                             <li>
                                 <a href="<?php echo get_site_url(); ?>/wp-content/uploads/2021/01/Fondide-vara-puhasvaartuse-maaramise-kord_01.01.2021.pdf" target="_blank"><?php _e('Procedure for determining net worth of fund (in Estonian)', TEXT_DOMAIN) ?></a>
-                            </li>
-                            <li>
-                                <a href="https://docs.google.com/document/d/1zK5L2eu0noXtDWWR-cCwGX_SYPazkJLMqYvgq8fjpuU/edit" target="_blank">Teade tingimuste muudatustest alates 05.03.2021</a>
                             </li>
                         </ul>
 
