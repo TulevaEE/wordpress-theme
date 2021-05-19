bc81952aa31cd273cf6b82233755b387b8a50ce7
Carl Rannaberg <carlrannaberg@gmail.com>
2021-05-19 15:28:55 +0300

    Make blog submenu term titles dynamic.

--- src/wp-content/themes/tuleva/templates/blog/submenu.php
+++ src/wp-content/themes/tuleva/templates/blog/submenu.php
@@ -12,19 +12,19 @@
             <a href="<?php echo get_permalink($page_id); ?>"><?php _e('All articles', TEXT_DOMAIN); ?></a>
         </li>
 
-        <?php if ($term_id) { ?>
+        <?php if ($term_id && get_term($term_id)) { ?>
             <li class="submenu__item<?php if ($term_id === $current_object_id) {
                     echo ' submenu__item--current';
                 } ?>">
-                <a href="<?php echo get_term_link($term_id); ?>"><?php _e('Featured articles', TEXT_DOMAIN); ?></a>
+                <a href="<?php echo get_term_link($term_id); ?>"><?php echo get_term($term_id)->name; ?></a>
             </li>
         <?php } ?>
 
-        <?php if ($secondary_term_id) { ?>
+        <?php if ($secondary_term_id && get_term($secondary_term_id)) { ?>
             <li class="submenu__item<?php if ($secondary_term_id === $current_object_id) {
                     echo ' submenu__item--current';
                 } ?>">
-                <a href="<?php echo get_term_link($secondary_term_id); ?>"><?php _e('Laura getting rich', TEXT_DOMAIN); ?></a>
+                <a href="<?php echo get_term_link($secondary_term_id); ?>"><?php echo get_term($secondary_term_id)->name; ?></a>
             </li>
         <?php } ?>
     </ul>
