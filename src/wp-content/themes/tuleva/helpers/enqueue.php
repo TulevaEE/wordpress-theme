308b65284a576c9e86435e17cd5dd1f07a0cd20b
Carl Rannaberg <carl@pipedrive.com>
2021-07-06 10:13:24 +0300

    Change cache breaker.

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -27,7 +27,7 @@ class ThemeEnqueue {
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '20210302',
+                'ver' => '20210706',
                 'media' => 'all',
                 'enqueue' => true
             ],
