e0606a9fa7e158f164418c966eac616621a2cb22
Carl Rannaberg <carlrannaberg@gmail.com>
2021-07-08 13:08:17 +0300

    Update reports.

--- src/wp-content/themes/tuleva/templates/components/fund-third-details.php
+++ src/wp-content/themes/tuleva/templates/components/fund-third-details.php
@@ -79,7 +79,7 @@
                             <li>
                                 <?php _e('Investment reports (in Estonian)', TEXT_DOMAIN) ?>
                                 <br>
-                                <a href="<?php echo get_site_url(); ?>/wp-content/uploads/2021/06/TUV100-investeeringute-aruanne-31.05.2021.pdf" target="_blank">05.21</a>
+                                <a href="<?php echo get_site_url(); ?>/wp-content/uploads/2021/07/TUV100-investeeringute-aruanne-30.06.2021.pdf" target="_blank">05.21</a>
                                 <br>
                                 <a href="https://www.pensionikeskus.ee/iii-sammas/vabatahtlikud-fondid/fid/81/" target="_blank"><?php _e('Previous reports', TEXT_DOMAIN) ?></a>
                             </li>
