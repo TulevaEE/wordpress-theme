7c8a42be5d2c052166198291e561f1a1e1376e4f
Carl Rannaberg <carl@pipedrive.com>
2021-09-15 18:52:14 +0300

    Update reports.

--- src/wp-content/themes/tuleva/templates/components/fund-bonds-details.php
+++ src/wp-content/themes/tuleva/templates/components/fund-bonds-details.php
@@ -79,7 +79,7 @@
                         <li>
                             <?php _e('Investment reports (in Estonian)', TEXT_DOMAIN) ?>
                             <br>
-                            <a href="<?php echo get_site_url(); ?>/wp-content/uploads/2021/08/TULVPF-investeeringute-aruanne-300721.pdf" target="_blank">07.21</a>
+                            <a href="<?php echo get_site_url(); ?>/wp-content/uploads/2021/09/TULVPF-investeeringute-aruanne-310821.pdf" target="_blank">08.21</a>
                             <br>
                             <a href="https://www.pensionikeskus.ee/ii-sammas/kohustuslikud-pensionifondid/fid/76/" target="_blank"><?php _e('Previous reports', TEXT_DOMAIN) ?></a>
                         </li>
--- src/wp-content/themes/tuleva/templates/components/fund-third-details.php
+++ src/wp-content/themes/tuleva/templates/components/fund-third-details.php
@@ -79,7 +79,7 @@
                             <li>
                                 <?php _e('Investment reports (in Estonian)', TEXT_DOMAIN) ?>
                                 <br>
-                                <a href="<?php echo get_site_url(); ?>/wp-content/uploads/2021/08/TUV100-investeeringute-aruanne-30.07.2021.pdf" target="_blank">07.21</a>
+                                <a href="<?php echo get_site_url(); ?>/wp-content/uploads/2021/09/TUV100-investeeringute-aruanne-31.08.2021.pdf" target="_blank">08.21</a>
                                 <br>
                                 <a href="https://www.pensionikeskus.ee/iii-sammas/vabatahtlikud-fondid/fid/81/" target="_blank"><?php _e('Previous reports', TEXT_DOMAIN) ?></a>
                             </li>
