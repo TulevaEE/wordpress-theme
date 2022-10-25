4f550008af08562a772d3978b219eca8674f1021
Carl Rannaberg <carl@pipedrive.com>
2022-10-25 08:42:52 +0300

    Update sustainability numbers.

--- src/wp-content/themes/tuleva/templates/components/fund-bonds-details.php
+++ src/wp-content/themes/tuleva/templates/components/fund-bonds-details.php
@@ -91,7 +91,7 @@
                         <h5 class="mb-4"><?php _e('Sustainability information', TEXT_DOMAIN) ?></h5>
                         <div class="fund-info__item">
                             <div class="small text-bold"><?php _e('CO2 intensity', TEXT_DOMAIN) ?></div>
-                            <?php echo sprintf(__('%s tons / $1M turnover per year', TEXT_DOMAIN), 156) ?>
+                            <?php echo sprintf(__('%s tons / $1M turnover per year', TEXT_DOMAIN), 160) ?>
                         </div>
                         <div class="fund-info__item">
                             <div class="small text-bold"><?php _e('Share of investments with increased negative impact in the equity portfolio', TEXT_DOMAIN) ?></div>
