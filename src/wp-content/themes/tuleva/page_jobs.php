ffa45fa324ad43d723581c9c588917e4a686fac9
Carl Rannaberg <carl@pipedrive.com>
2023-02-25 22:37:13 +0200

    Add jobs page template.

---
+++ src/wp-content/themes/tuleva/page_jobs.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Template Name: Jobs
+*/
+?>
+<?php get_header(); ?>
+<?php get_template_part( 'templates/jobs-content' ); ?>
+<?php get_footer(); ?>
---
+++ src/wp-content/themes/tuleva/templates/jobs-content.php
@@ -0,0 +1,220 @@
+<div class="page-container">
+    <section class="bg-hero-main d-flex flex-column">
+        <div class="container my-auto">
+            <div class="row align-items-center py-5">
+                <div class="col-lg-6 mx-auto">
+                    <h1 class="mb-5">Tuleva revolutsioon kogub hoogu</h1>
+                     <p class="lead text-navy mb-3">Ehita koos meiega Eesti mõjusaim investeerimisettevõte</p>
+                    <p class="text-navy mb-5">Tuleva otsib Tõnu Peki ja Erko Ristheina kõrvale kahte uut juhatuse liiget, kellega koos asume oma mõjumudelit skaleerima ja kui meie liikmed nii otsustavad, hakkame ette valmistama Tuleva börsileviimist.</p>
+                </div>
+                <div class="col-lg-6">
+                    <img class="img-fluid" src="http://localhost:8888/tuleva/git/wordpress-theme/src/wp-content/uploads/2023/02/Tuleva_veebr23-23-e1676921839339.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="d-flex flex-column">
+        <div class="container my-auto">
+            <div class="row align-items-center py-5">
+                <div class="col-lg-6 text-center mt-5">
+                    <div class="col-12 pb-2">
+                        <img class="w-25 image-circle mb-2" src="http://localhost:8888/tuleva/git/wordpress-theme/src/wp-content/uploads/2023/02/Tuleva_veebr23-23-e1676921839339.jpg" alt="">
+                        <h3>Operatsioonide juht</h3>
+                        <a class="btn btn-link btn-plus collapsed" data-toggle="collapse" href="#job-description-1">
+                            <span class="text-navy lead font-weight-bold collapsed__shown">Loe lähemalt</span>
+                            <span class="text-navy lead font-weight-bold collapsed__hidden">Sulge</span>
+                        </a>
+                    </div>
+                    <hr class="w-50 d-inline-block">
+                    <div class="col-12 py-4">
+                        <a href="#" class="btn btn-primary btn-lg">Kandideeri</a>
+                    </div>
+                </div>
+                <div class="col-lg-6 text-center mt-5">
+                    <div class="col-12 pb-2">
+                        <img class="w-25 image-circle mb-2" src="http://localhost:8888/tuleva/git/wordpress-theme/src/wp-content/uploads/2023/02/Tuleva_veebr23-23-e1676921839339.jpg" alt="">
+                        <h3>Kasvustrateeg</h3>
+                        <a class="btn btn-link btn-plus collapsed" data-toggle="collapse" href="#job-description-2">
+                            <span class="text-navy lead font-weight-bold collapsed__shown">Loe lähemalt</span>
+                            <span class="text-navy lead font-weight-bold collapsed__hidden">Sulge</span>
+                        </a>
+                    </div>
+                    <hr class="w-50 d-inline-block">
+                    <div class="col-12 py-4">
+                        <a href="#" class="btn btn-primary btn-lg">Kandideeri</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="job-description-1" class="collapse bg-blue-washed">
+        <div class="container my-auto">
+            <div class="row align-items-center py-5">
+                <div class="column-2">
+                    <h2 class="h1 mb-5">Operatsioonide juht</h2>
+                    <h3>Sinu missioon Tulevas</h3>
+                    <p>Tuleva tööprotsesside optimaalne ja reeglitele vastava toimimise tagamine, et meie hoolde usaldatud vara oleks turvaliselt hoitud ja meie inimesed saaksid takistusteta teha head tööd.</p>
+                    <h3>Põhilised fookused ja ülesanded</h3>
+                    <ul>
+                        <li>Ettevõtte riskide kaardistamine ja maandamine</li>
+                        <li>Finantsjuhtimine</li>
+                        <li>Olemasoleva operatsioonide tiimi töö koordineerimine ja nendega koos:</li>
+                        <li>Juhtimisaruandlus</li>
+                        <li>Vastavuskontroll</li>
+                        <li>Juriidika</li>
+                        <li>Infrastruktuuri haldus</li>
+                        <li>Liikmete administreerimine</li>
+                        <li>Töötajate ja töökeskkonnaga seotud operatsioonilised tegevused</li>
+                        <li>Andme- ja tehnoloogiaturbe nõuete kirjeldamine ja nõuete vastavuse monitoorimine</li>
+                        <li>Toodetega seotud operatsioonilised tegevused</li>
+                    </ul>
+                    <p>Sinu roll on olla eestvedaja, kes aitaks kasvatada ettevõtet ja toetada uusi ärisuundi, tehes õigeid asju ausalt ning läbipaistvalt.</p>
+                    <p>Otsime inimest, kes ei teeks ära pelgalt vajamineva, vaid kelle ambitsioon oleks luua uusi norme ja standardeid.</p>
+                    <h3>Sinu edukuse mõõdikud</h3>
+                    <ul>
+                        <li>Tuleva töötajate rahulolu on kõrge ja ülesanded edukalt täidetud.</li>
+                        <li>Tuleva tooted toimivad tõrgeteta.</li>
+                        <li>Ettevõtte tegevusega seotud riskid on edukalt maandatud ja kohustused hoolsalt täidetud. Tulevat usaldatakse.</li>
+                        <li>Tuleva kulud on madalad, püsime alati hinnaliidrite seas.</li>
+                    </ul>
+                    <h3>Rollis õnnestumiseks on vajalik</h3>
+                    <ul>
+                        <li>Kogemus investeerimisteenuste administreerimises ja protsesside juhtimises</li>
+                        <li>Teadmised infoturbest, juriidikast, vastavuskontrollist, riskijuhtimisest.</li>
+                        <li>Süstemaatilisus ja detailidele orienteeritus</li>
+                    </ul>
+                    <p>Tasuvahemik:  5000-7000 € bruto + aastane boonus</p>
+                </div>
+                <div class="col-12 pt-5 text-center">
+                    <a href="#" class="btn btn-primary btn-lg">Kandideeri</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="d-flex flex-column">
+        <div class="container container my-auto">
+            <div class="row align-items-center py-5">
+                <div class="col-lg-6">
+                    <img class="img-fluid" src="http://localhost:8888/tuleva/git/wordpress-theme/src/wp-content/uploads/2023/02/Tuleva_veebr23-23-e1676921839339.jpg" alt="">
+                </div>
+                <div class="col-lg-6 mx-auto">
+                    <h2 class="h1 mb-1">Mis toimub?</h2>
+                    <p class="lead mb-3">Pealtnäha tegeleme Tulevas maailma kõige igavama asjaga: pensioniga. Miks?! </p>
+                    <p>Sellepärast, et kui pensionisambad hästi tööle panna, on need targad investeerimise tööriistad. Maksuvõit annab neile suure edumaa muude investeerimisvõimaluste ees.</p>
+                    <p>Tuleva ühendab ärksaid inimesi, kes ongi ehitanud oma finantsmajale pensionisammaste abil  tugeva vundamendi.</p>
+                    <p>Aga me oleme täna natuke selline “tarkade klubi” - tüüpiline Tulevas koguja on keskmisest kõrgema haridustaseme, finantskirjaoskuse ja sissetulekuga.</p>
+                    <p>Tuleva otsib Tõnu Peki ja Erko Ristheina kõrvale kahte uut juhatuse liiget, kellega koos asume oma mõjumudelit skaleerima ja kui meie liikmed nii otsustavad, hakkame ette valmistama Tuleva börsileviimist.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="d-none d-md-block py-6 bg-blue-washed">
+        <div class="container">
+        <h2 class="mb-5">Samal ajal</h2>
+        <ul class="list-style-arrow mb-5 lead">
+            <li class="mb-3">maksab 80% teises sambas kogujatest ikka veel pankadele vanades fondides kõrget tasu - see on umbes 450 000 inimest;</li>
+            <li class="mb-3">kolmandat sammast kasutab maksuvõimendusega investeerimiseks ainult 130 000 inimest - neist kolmandik kogub Tulevas;</li>
+            <li class="mb-3">Enamusele Eesti inimestest on teise samba vara peamiseks finantsvaraks või pole sedagi.</li>
+        </div>
+    </section>
+    <section class="d-flex flex-column">
+        <div class="container container my-auto">
+            <div class="row align-items-center py-5">
+                <div class="col-lg-6 mx-auto">
+                    <h3 class="mb-5">Tarkade klubist jõukamasse ühiskonda</h3>
+                    <p>Eesti elu on parem, kui meist võimalikult paljud sihikindlalt oma tuleviku heaks kapitali koguvad. Siis on vähem hädasolijaid, kelle toimetulek koormab meie lapsi. See ongi Tuleva missioon.</p>
+                    <p>Juba täna kogub Tulevas ligi 65 000 inimest. Aga vaid mõni tuhat neist on sihikindlad kogujad, kes investeerivad teise ja kolmanda samba abil oma tulevikku igakuisest sissetulekust vähemalt 15% või rohkem.</p>
+                </div>
+                <div class="col-lg-6">
+                    <img class="img-fluid" src="http://localhost:8888/tuleva/git/wordpress-theme/src/wp-content/uploads/2023/02/Tuleva_veebr23-23-e1676921839339.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="d-flex flex-column">
+        <div class="container container my-auto">
+            <div class="row align-items-center py-5">
+                TODO: Job opening descriptions section
+            </div>
+        </div>
+    </section>
+    <section class="d-flex flex-column bg-blue-washed">
+        <div class="container container my-auto">
+            <div class="row align-items-center py-5">
+                <div class="col-lg-12 mx-auto">
+                    <h3 class="mb-5">Uute juhtide väljakutse: võimsast kodanikualgatusest edukaks börsiettevõtteks?</h3>
+                    <p>Senised saavutused oleme koju toonud ülikonservatiivselt tegutsedes. Kas me oleme ennast piisavalt tõestanud, et varsti julgemalt tulevikku investeerida?</p>
+                    <p>Et kaasata investoreid, motiveerida töötajaid ja innustada oma kogukonna liikmeid ühise edu nimel pingutama, peab olema selge, kuidas nemad Tuleva kasvust kasu saavad. Kõige selgem tee on Tuleva börsileviimine. Selleks, peame koos liikmetega läbi arutama ja tegema meie ühistu tulevikku mõjutavad põhimõttelised valikud.</p>
+                    <p>Idee, et Tuleva ühistu võiks kaaluda oma fondiäri börsileviimist, tekitab esialgu õigustatud küsimuse: kas see ei ole vastuolus meie algsete põhimõtetega?</p>
+                    <p>Tuleva ei tohi kunagi hakata mängima nullsummamängu meie fondides kogujatega. Kui kogujatel läheb hästi, läheb meil hästi. Kui unustame missiooni, kaotame oma eelise. Ja kui Tuleva on edukas, läheb Eesti elu paremaks.</p>
+                    <p>Meie nõukogu esimees Kristi Saare sõnastas selle ühel töisel koosolekul möödaminnes kõige paremini: “Me tõestasime, et saab teha paremaid fonde. Tõestame nüüd, et saab teha paremat börsiettevõtet.”</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="d-flex flex-column">
+        <div class="container container my-auto">
+            <div class="row align-items-center py-5">
+                <div class="col-lg-6">
+                    <img class="img-fluid" src="http://localhost:8888/tuleva/git/wordpress-theme/src/wp-content/uploads/2023/02/Tuleva_veebr23-23-e1676921839339.jpg" alt="">
+                </div>
+                <div class="col-lg-6 mx-auto">
+                    <h3 class="mb-5">Tõnu: tiim vajab rohkem võimsust</h3>
+                    <p>Oleme nõelterava fookuse ja liikmete aktiivse osalusega kasvatanud Tuleva palju suuremaks kui meie väike tiim. Nüüd on aeg aidata iseennast järgi, et suudaksime seniseid saavutusi hüppeliselt võimendada.</p>
+                    <p>Ma ei otsi alluvaid, vaid juhte enda kõrvale.</p>
+                    <p>Loodan, et uus operatsioonide juht toob lauale skaleerimisfaasis ettevõtte tiimi arendamise kogemuse. Kuidas leida parimad inimesed? Millised protsessid tuleb paika saada, et meie töökeskkond ja kultuur laseks neil õitseda?</p>
+                    <p>Kasvustrateeg peab pidevalt otsima viise infomürast segadusse aetud inimeste tähelepanu koondamiseks. Ikka selleks, et nad saaksid ilma aega raiskamata efektiivselt oma finantseesmärkide poole liikuda. Kuidas innustada tänaseid teadlikke Tuleva fänne, et nad juhiksid ka sõbrad-sugulased enesekindlalt raha koguma? Kuidas haarata kinni väikestest ja suurtest võimalustest, raiskamata raha hiiglaslikele turunduseelarvetele ja sisutühjadele reklaamidele?</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="d-flex flex-column bg-blue-washed">
+        <div class="container container my-auto">
+            <div class="row align-items-center py-5">
+                <div class="col-lg-6 mx-auto">
+                    <h3 class="mb-5">Erko: et missiooni täita, peame kõik kasvule mõtlema</h3>
+                    <p>Madala tasuga indeksifondides kogub alles napilt üle viiendiku inimestest. See tähendab, et 80% missioonist on meil veel täitmata. Kogu Tuleva tiim peab seega olema kasvule orienteeritud. </p>
+                    <p>Me kõik peame igapäevaselt kasutajatega suhtlema, et õppida nende vajadusi mõistma, ja töötama tihedalt koos, et inimeste teelt takistusi kõrvaldada. Kõik tegijad kasutajatoest vastavuskontrollini peavad keskenduma asjadele, millest inimestele päriselt kasu on. Itereerime samm-sammult nii tootearenduses kui ka meeskonnatöös ja kommunikatsioonis. </p>
+                    <p>Tulevas hoolitsevad kõik, et asjad saaksid tehtud. Kui vaja, koristan kontorit; kui vaja, kohtun poliitikakujundajatega; kui vaja, kirjutan paar rida koodi jne. Me ei kohku ülesannete ees, mida varem teinud pole, ja ei karda tunnistada, et me kõike ei tea. Mõõdame tehtud tööd. Kehvad tulemused on õppimiseks ja head tulemused võimendamiseks. </p>
+                    <p>Kiire kasv tähendab seda, et oleme pidevalt muutumises. Juhtidelt, kes minu ja Tõnu kõrvale tulevad, ootan mitte meie otsuste ootele jäämist, vaid ise eeskuju ja tegudega Tuleva töökultuuri aktiivset kujundamist.</p>
+                </div>
+                <div class="col-lg-6">
+                    <img class="img-fluid" src="http://localhost:8888/tuleva/git/wordpress-theme/src/wp-content/uploads/2023/02/Tuleva_veebr23-23-e1676921839339.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="d-flex flex-column">
+        <div class="container container my-auto">
+            <div class="row align-items-center py-5">
+                <div class="col-lg-6">
+                    <img class="img-fluid" src="http://localhost:8888/tuleva/git/wordpress-theme/src/wp-content/uploads/2023/02/Tuleva_veebr23-23-e1676921839339.jpg" alt="">
+                </div>
+                <div class="col-lg-6 mx-auto">
+                    <h3 class="mb-5">Tuleva pakub:</h3>
+                    <ul>
+                        <li>Saad lõpuks oma kogemusi, annet ja kujutlusvõimet tulemuslikult rakendada. Sinu kätes on ehitada investeerimisettevõte, mis päriselt inimeste elu paremaks teeb.</li>
+                        <li>Saad tulemusliku töö eest väga head tasu ja muidugi aktsiaoptsioone kui otsustame koos IPO teele minna.</li>
+                        <li>Meie töötajad soodustavad õppimist soodustavat töökorraldust ja igaühe head elu toetavat paindlikku töökultuuri. Kui sa millestki puudust tunned, teeme selle koos korda.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+     <section class="d-flex flex-column">
+        <div class="container container my-auto">
+            <div class="row align-items-center py-5">
+                <div class="col-lg-12 text-center px-6">
+                    <p class="lead">Tutvu esialgsete ametikirjeldustega. Kui kumbki neist tundub enamvähem sinu territoorium, anna endast märku ja arutame lähemalt. Peamine on, et neljakesi koos jagame omavahel kõik Tuleva edukaks juhtimiseks vajalikud rollid.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="d-flex flex-column">
+        <div class="container container my-auto">
+            <div class="row align-items-center py-5">
+                TODO: Job opening descriptions section
+            </div>
+        </div>
+    </section>
+</div>
