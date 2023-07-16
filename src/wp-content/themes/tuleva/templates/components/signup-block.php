021f0432aafe80adb5189f072dcab62da342b9fe
Carl Rannaberg <carl@pipedrive.com>
2023-07-16 21:51:34 +0300

    Add text field below signup block heading.

--- src/wp-content/themes/tuleva/templates/components/signup-block.php
+++ src/wp-content/themes/tuleva/templates/components/signup-block.php
@@ -1,9 +1,10 @@
 <section id="<?php the_sub_field('component_id'); ?>">
     <div id="inline-signup-anchor"></div>
     <div class="container row-spacing-half">
-        <div class="row text-center row-spacing-bottom-quarter">
+        <div class="row row-spacing-bottom-quarter">
             <div class="col">
-                <h2><?php the_sub_field('heading'); ?></h2>
+                <h2 class="text-center"><?php the_sub_field('heading'); ?></h2>
+                <?php the_sub_field('text'); ?>
             </div>
         </div>
         <div class="row">
