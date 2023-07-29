b6d4594150771119088a149781a9ee7121c4cf25
Carl Rannaberg <carl@pipedrive.com>
2023-07-29 21:37:58 +0300

    Hide rich text block without content.

--- src/wp-content/themes/tuleva/templates/components/rich-text-block.php
+++ src/wp-content/themes/tuleva/templates/components/rich-text-block.php
@@ -1,7 +1,11 @@
-<div id="<?php the_sub_field('component_id'); ?>" class="<?php echo get_component_classes('container'); ?>">
-    <div class="row">
-        <div class="col-md-8 offset-md-2">
-            <?php the_sub_field('text'); ?>
+<?php $text = get_sub_field('text'); ?>
+<?php if (!$text) return; ?>
+<section id="<?php the_sub_field('component_id'); ?>" class="py-6">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <?php echo $text; ?>
+            </div>
         </div>
     </div>
-</div>
+</section>
