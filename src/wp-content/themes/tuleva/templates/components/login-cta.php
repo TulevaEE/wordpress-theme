a918ad3d17c4880137dde22c0ad10d1618acf6a8
Carl Rannaberg <carl@pipedrive.com>
2023-09-12 23:01:37 +0300

    Add checks for content.

--- src/wp-content/themes/tuleva/templates/components/login-cta.php
+++ src/wp-content/themes/tuleva/templates/components/login-cta.php
@@ -9,28 +9,34 @@ $bg_class = get_component_background_color_class();
                     <h1><?php the_sub_field('heading'); ?></h1>
                 </div>
                 <div class="col-lg-6">
-                    <p class="lead text-navy"><?php the_sub_field('lead_text'); ?></p>
+                    <?php if (get_sub_field('lead_text')): ?>
+                        <p class="lead text-navy"><?php the_sub_field('lead_text'); ?></p>
+                    <?php endif; ?>
                     <?php the_sub_field('text'); ?>
                 </div>
-                <div class="col-lg-6">
-                    <div class="d-flex justify-content-center mb-2">
-                        <div class="p-2">
-                            <img src="<?php echo get_template_directory_uri() ?>/img/id-card-logo.png" style="max-height:32px;" class="img-fluid" alt="Logo 1">
+                <?php if (get_sub_field('button_text') && get_sub_field('button_url')): ?>
+                    <div class="col-lg-6">
+                        <div class="d-flex justify-content-center mb-2">
+                            <div class="p-2">
+                                <img src="<?php echo get_template_directory_uri() ?>/img/id-card-logo.png" style="max-height:32px;" class="img-fluid" alt="Logo 1">
+                            </div>
+                            <div class="p-2">
+                                <img src="<?php echo get_template_directory_uri() ?>/img/mobile-id-logo.png" style="max-height:32px;" class="img-fluid" alt="Logo 2">
+                            </div>
+                            <div class="p-2">
+                                <img src="<?php echo get_template_directory_uri() ?>/img/smart-id-logo.png" style="max-height: 32px;" class="img-fluid" alt="Logo 3">
+                            </div>
                         </div>
-                        <div class="p-2">
-                            <img src="<?php echo get_template_directory_uri() ?>/img/mobile-id-logo.png" style="max-height:32px;" class="img-fluid" alt="Logo 2">
-                        </div>
-                        <div class="p-2">
-                            <img src="<?php echo get_template_directory_uri() ?>/img/smart-id-logo.png" style="max-height: 32px;" class="img-fluid" alt="Logo 3">
+                        <div class="text-center">
+                            <a href="<?php the_sub_field('button_url'); ?>" class="btn btn-primary btn-lg mb-3"><?php the_sub_field('button_text'); ?></a>
+                            <?php if (get_sub_field('small_text')): ?>
+                                <p class="small text-navy mb-md-5 mb-lg-0">
+                                    <?php the_sub_field('small_text'); ?>
+                                </p>
+                            <?php endif; ?>
                         </div>
                     </div>
-                    <div class="text-center">
-                        <a href="<?php the_sub_field('button_url'); ?>" class="btn btn-primary btn-lg mb-3"><?php the_sub_field('button_text'); ?></a>
-                        <p class="small text-navy mb-md-5 mb-lg-0">
-                            <?php the_sub_field('small_text'); ?>
-                        </p>
-                    </div>
-                </div>
+                <?php endif; ?>
             </div>
         </div>
     </div>
