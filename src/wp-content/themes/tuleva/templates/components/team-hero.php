1d383a369317d3c06606632e91d403cee2d2b9d6
Carl Rannaberg <carl@pipedrive.com>
2023-09-17 21:36:26 +0300

    Add button to tema hero.

--- src/wp-content/themes/tuleva/templates/components/team-hero.php
+++ src/wp-content/themes/tuleva/templates/components/team-hero.php
@@ -11,6 +11,14 @@ $image_srcset = wp_get_attachment_image_srcset($image['ID'],'large');
                 <h1 class="mb-5"><?php the_sub_field('heading'); ?></h1>
                     <p class="lead text-navy mb-3"><?php the_sub_field('lead_text'); ?></p>
                 <div class="text-navy mb-5"><?php the_sub_field('text'); ?></div>
+                <?php if (get_sub_field('button_text')) { ?>
+                    <a href="<?php the_sub_field('button_url'); ?>" class="btn btn-primary btn-block btn-lg mb-3"><?php the_sub_field('button_text'); ?></a>
+                    <?php if (get_sub_field('small_text')): ?>
+                        <p class="small text-navy mb-md-5 mb-lg-0">
+                            <?php the_sub_field('small_text'); ?>
+                        </p>
+                    <?php endif; ?>
+                <?php } ?>
             </div>
             <div class="col-lg-6">
                 <img class="img-fluid" src="<?php echo $image_url; ?>" srcset="<?php echo $image_srcset; ?> alt="">
