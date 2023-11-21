762e7c8500d4eb6a79a6f4923ad6b09bf450a044
Carl Rannaberg <carl@pipedrive.com>
2023-11-21 09:13:45 +0200

    Bump cachebreaker.

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -31,7 +31,7 @@ class ThemeEnqueue
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '20231119',
+                'ver' => '20231121',
                 'media' => 'all',
                 'enqueue' => true
             ],
