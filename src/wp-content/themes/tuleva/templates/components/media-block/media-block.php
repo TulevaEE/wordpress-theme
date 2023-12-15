081404b3db42c8edcc409253af9a2d93ed784ba1
Carl Rannaberg <carl@pipedrive.com>
2023-12-15 19:45:14 +0200

    More space between image and content in media block.

--- src/wp-content/themes/tuleva/templates/components/media-block/media-block.php
+++ src/wp-content/themes/tuleva/templates/components/media-block/media-block.php
@@ -5,6 +5,8 @@ $original_image_url = get_sub_field('image');
 $image_id = attachment_url_to_postid($original_image_url);
 $image_url = wp_get_attachment_image_url($image_id, 'large');
 $content_class = $image_url ? 'col-lg-6' : 'col-lg-12';
+$content_spacing_class = $image_alignment === 'right' ? 'pr-lg-4' : 'pl-lg-4';
+$image_spacing_class = $image_alignment === 'right' ? 'pl-lg-4' : 'pr-lg-4';
 $left_image_visibility_class = $image_url && $image_alignment === 'right' ? 'd-block d-lg-none' : '';
 $right_image_visibility_class = $image_url && $image_alignment === 'right' ? 'd-none d-lg-block' : '';
 ?>
@@ -12,12 +14,12 @@ $right_image_visibility_class = $image_url && $image_alignment === 'right' ? 'd-
     <div class="container my-auto">
         <div class="row align-items-center py-5">
             <?php if ($image_url) { ?>
-                <div class="col-lg-6 <?php echo $left_image_visibility_class; ?>">
+                <div class="col-lg-6 <?php echo $left_image_visibility_class; ?> <?php echo $image_spacing_class; ?>">
                     <?php get_template_part('templates/components/media-block/media-block-image'); ?>
                 </div>
             <?php } ?>
 
-            <div class="<?php echo $content_class ?> mx-auto">
+            <div class="<?php echo $content_class ?> <?php echo $content_spacing_class; ?> mx-auto">
                 <h3 class="mb-4"><?php the_sub_field('heading') ?></h3>
                 <?php if (get_sub_field('lead_text')): ?>
                     <p class="lead mb-3"><?php the_sub_field('lead_text') ?></p>
@@ -26,7 +28,7 @@ $right_image_visibility_class = $image_url && $image_alignment === 'right' ? 'd-
             </div>
 
             <?php if ($image_url && $image_alignment === 'right'): ?>
-                <div class="col-lg-6 <?php echo $right_image_visibility_class; ?>">
+                <div class="col-lg-6 <?php echo $right_image_visibility_class; ?> <?php echo $image_spacing_class; ?>">
                     <?php get_template_part('templates/components/media-block/media-block-image'); ?>
                 </div>
             <?php endif; ?>
