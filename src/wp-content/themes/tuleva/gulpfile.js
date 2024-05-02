2cb1dd4a9a1f91114326454b6a737c928f5da8ea
Carl Rannaberg <carl@pipedrive.com>
2024-05-02 09:42:56 +0300

    Add blog pagination.

--- src/wp-content/themes/tuleva/gulpfile.js
+++ src/wp-content/themes/tuleva/gulpfile.js
@@ -1,60 +1,83 @@
-var gulp = require('gulp'),
-    sourcemaps = require('gulp-sourcemaps'),
-    gulpif = require('gulp-if'),
-    sass = require('gulp-sass')(require('sass')),
-    cleanCSS = require('gulp-clean-css'),
-    autoprefixer = require('gulp-autoprefixer'),
-    watch = require('gulp-watch'),
-    log = require('fancy-log'),
-    exit = require('gulp-exit'),
-    cssDir = 'css',
-    sassSrc = 'scss/**/*.scss',
-    error = function(err) {
-        if (err) {
-            log(err.toString());
-        }
+const gulp = require("gulp");
+const sourcemaps = require("gulp-sourcemaps");
+const gulpif = require("gulp-if");
+const sass = require("gulp-sass")(require("sass"));
+const cleanCSS = require("gulp-clean-css");
+const autoprefixer = require("gulp-autoprefixer");
+const watch = require("gulp-watch");
+const log = require("fancy-log");
+const exit = require("gulp-exit");
+const cssDir = "css";
+const sassSrc = "scss/**/*.scss";
 
-        if (this.emit) {
-            this.emit('end');
-        }
-    },
-    logFinishedTask = function(name) {
-        return function() {
-            log(name + ' task finished.');
-        };
-    },
-    buildCss = function(isCompressed) {
-        return gulp.src(sassSrc)
-            .pipe(gulpif(!isCompressed, sourcemaps.init()))
-            .pipe(sass({ outputStyle: isCompressed ? 'compressed' : 'expanded' })
-                            .on('end', logFinishedTask('SASS'))
-                            .on('error', error))
-            .pipe(gulpif(isCompressed, cleanCSS({level: 2})
-                            .on('end', logFinishedTask('CleanCSS'))
-                            .on('error', error)))
-            .pipe(autoprefixer({
-                    overrideBrowserslist: ['> 1%', 'last 2 versions', 'safari >= 7'],
-                    cascade: false
-                })
-                .on('end', logFinishedTask('Autoprefixer'))
-                .on('error', error))
-            .pipe(gulpif(!isCompressed, sourcemaps.write('.')
-                            .on('end', logFinishedTask('Sourcemaps'))
-                            .on('error', error)))
-            .pipe(gulp.dest(cssDir))
-            .pipe(gulpif(isCompressed, exit()));
+const onError = function (err) {
+    if (err) {
+        log(err.toString());
+    }
+
+    if (this.emit) {
+        this.emit("end");
+    }
+};
+
+const logFinishedTask = function (name) {
+    return function () {
+        log(name + " task finished.");
     };
+};
 
-gulp.task('production', function() {
-  return buildCss(true);
+const buildCss = function (isCompressed) {
+    return gulp
+        .src(sassSrc)
+        .pipe(gulpif(!isCompressed, sourcemaps.init()))
+        .pipe(
+            sass({ outputStyle: isCompressed ? "compressed" : "expanded" })
+                .on("end", logFinishedTask("SASS"))
+                .on("error", onError)
+        )
+        .pipe(
+            gulpif(
+                isCompressed,
+                cleanCSS({ level: 2 })
+                    .on("end", logFinishedTask("CleanCSS"))
+                    .on("error", onError)
+            )
+        )
+        .pipe(
+            autoprefixer({
+                overrideBrowserslist: [
+                    "> 1%",
+                    "last 2 versions",
+                    "safari >= 7",
+                ],
+                cascade: false,
+            })
+                .on("end", logFinishedTask("Autoprefixer"))
+                .on("error", onError)
+        )
+        .pipe(
+            gulpif(
+                !isCompressed,
+                sourcemaps
+                    .write(".")
+                    .on("end", logFinishedTask("Sourcemaps"))
+                    .on("error", onError)
+            )
+        )
+        .pipe(gulp.dest(cssDir))
+        .pipe(gulpif(isCompressed, exit()));
+};
+
+gulp.task("production", function () {
+    return buildCss(true);
 });
 
-gulp.task('default', function() {
-    return watch(sassSrc, function() {
+gulp.task("default", function () {
+    return watch(sassSrc, function () {
         return buildCss(false);
     })
-    .on('change', function(path) {
-        log('Changed: ' + path);
-    })
-    .on('error', error);
+        .on("change", function (path) {
+            log("Changed: " + path);
+        })
+        .on("error", onError);
 });
--- src/wp-content/themes/tuleva/templates/blog/pagination.php
+++ src/wp-content/themes/tuleva/templates/blog/pagination.php
@@ -1,10 +1,1 @@
-<?php if ($paged > 1) { ?>
-    <div class="pagination">
-        <div class="pagination__previous"><?php next_posts_link(__('Older articles', TEXT_DOMAIN)); ?></div>
-        <div class="pagination__next"><?php previous_posts_link(__('Newer articles', TEXT_DOMAIN)); ?></div>
-    </div>
-<?php } else { ?>
-    <div class="pagination">
-        <div class="pagination__previous"><?php next_posts_link(__('Older articles', TEXT_DOMAIN)); ?></div>
-    </div>
-<?php } ?>
+<?php blog_pagination(); ?>
