44b9c47d799239bd1eb5daa281d2a4fb8660782f
Carl Rannaberg <carl@pipedrive.com>
2024-05-24 10:45:04 +0300

    Add full width option to rich text block component.

--- src/wp-content/themes/tuleva/templates/components/rich-text-block.php
+++ src/wp-content/themes/tuleva/templates/components/rich-text-block.php
@@ -1,10 +1,11 @@
 <?php
 $bg_class = get_component_background_color_class();
 $text = get_sub_field('text');
+$container_class = get_sub_field('is_full_width') ? 'container-fluid' : 'container';
 ?>
 <?php if (!$text) return; ?>
 <section id="<?php the_sub_field('component_id'); ?>" class="py-6 <?php echo $bg_class; ?>">
-    <div class="container">
+    <div class="<?php echo $container_class; ?>">
         <div class="row">
             <div class="col-md-8 offset-md-2">
                 <?php echo $text; ?>
