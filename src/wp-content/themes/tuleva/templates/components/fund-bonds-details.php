5f45a9d37051cdeb8319f3a5f617efe802af47d1
Carl Rannaberg <carl@pipedrive.com>
2024-06-06 16:30:10 +0300

    Update reports.

--- src/wp-content/themes/tuleva/templates/components/fund-bonds-details.php
+++ src/wp-content/themes/tuleva/templates/components/fund-bonds-details.php
@@ -92,7 +92,7 @@
                             <li>
                                 <?php _e('Investment reports (in Estonian)', TEXT_DOMAIN) ?>
                                 <br>
-                                <?php echo generate_report_link('https://tuleva.ee/wp-content/uploads/2024/05/TULVPF-investeeringute-aruanne-300424.pdf'); ?>
+                                <?php echo generate_report_link('https://tuleva.ee/wp-content/uploads/2024/06/TULVPF-investeeringute-aruanne-310524.pdf'); ?>
                                 <br>
                                 <a href="https://www.pensionikeskus.ee/ii-sammas/kohustuslikud-pensionifondid/fid/76/" target="_blank"><?php _e('Previous reports', TEXT_DOMAIN) ?></a>
                             </li>
