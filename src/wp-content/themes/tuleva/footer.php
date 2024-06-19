4d41858e694bfd5a1f5092d77cb3b735d28e0167
Sander Nemvalts <snemvalts@gmail.com>
2024-06-19 08:23:13 +0300

    Give more horizontal space on tight mobile screens

--- src/wp-content/themes/tuleva/footer.php
+++ src/wp-content/themes/tuleva/footer.php
@@ -1,6 +1,6 @@
 <footer class="footer">
-    <?php if (isset($_GET['development']) && $_GET['development'] === 'true') : ?>
-        <div class="d-flex align-items-center justify-content-center">
+    <?php if (isset($_GET['development'])) : ?>
+        <div class="px-3 d-flex align-items-center justify-content-center">
             <?php get_template_part('templates/footer/not-logged-in-mailing-list-form'); ?>
         </div>
     <?php endif; ?>
