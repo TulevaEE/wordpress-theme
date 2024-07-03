1ed4f4352823ca28b240bfc30d562077d435841c
Erko Risthein <erko@risthein.ee>
2024-07-03 15:34:52 +0300

    Bump main.js cache breaker

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -103,7 +103,7 @@ class ThemeEnqueue
                 'handle' => 'main-script',
                 'src' => $js_path . 'main.js',
                 'deps' => 'jquery',
-                'ver' => '20240702_3',
+                'ver' => '20240703',
                 'js_in_header' => false,
                 'enqueue' => true
             ]
