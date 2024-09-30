f27d8ba512aab4f5b7cef3855516cf0b3c692a58
Taimar <tteetlok@gmail.com>
2024-09-30 15:11:36 +0300

    Payout calculator design and text tweaks

--- src/wp-content/themes/tuleva/templates/components/payout-calculator.php
+++ src/wp-content/themes/tuleva/templates/components/payout-calculator.php
@@ -3,7 +3,7 @@
 
 <div class="card shadow-md" style="border-radius: 1rem">
     <div class="card-body p-2 payout-calculator">
-        <div class="card bg-light p-3" style="border-radius: 1rem">
+        <div class="card bg-light p-3" style="border-radius: .5rem">
             <div class="form-group row">
                 <label for="portfolioSum" class="col-sm-6 col-form-label vertical-align pr-0">
                     <?php _e('Accumulated by age 65', TEXT_DOMAIN); ?>
@@ -34,24 +34,21 @@
         </div>
         <div class="results row m-0">
             <div class="col-6 pl-0 pr-1">
-                <div class="card bg-blue-washed p-3 mt-2 text-center text-nowrap" style="border-radius: 1rem">
+                <div class="card bg-blue-washed p-3 py-4 mt-2 text-center text-nowrap" style="border-radius: .5rem">
                     <b><?php _e('You will receive monthly', TEXT_DOMAIN); ?></b>
-                    <div>
-                        <span id="recurringPayoutMonthly1" class="h4 text-primary mb-0">88</span><span
-                            id="recurringArrow" class="h4 text-primary mb-0 d-none"
-                            style="font-family: system-ui; font-size: 1.2rem">→</span><span id="recurringPayoutMonthly2"
-                                                                                            class="h4 text-primary mb-0 d-none">88</span>
-                        <span id="recurringEuro" class="h4 text-primary mb-0"> €</span>
+                    <div class="h3 m-0 text-primary">
+                        <span id="recurringPayoutMonthly1">88</span><span id="recurringArrow" class="d-none" style="font-family: system-ui;">→</span><span id="recurringPayoutMonthly2" class="d-none">88</span>
+                        <span id="recurringEuro"> €</span>
                     </div>
                 </div>
             </div>
             <div class="col-6 pl-1 pr-0">
-                <div class="card bg-blue-washed p-3 mt-2 text-center" style="border-radius: 1rem">
+                <div class="card bg-blue-washed p-3 py-4 mt-2 text-center" style="border-radius: .5rem">
                     <b><?php _e('You will receive in total', TEXT_DOMAIN); ?></b>
-                    <div id="recurringPayoutSum" class="h4 text-primary mb-0">20 000 €</div>
+                    <div id="recurringPayoutSum" class="h3 m-0 text-primary">20 000 €</div>
                 </div>
             </div>
         </div>
-        <div class="small text-muted m-3 text-center">Statistiliselt on 65-aastasel inimesel elada jäänud <b>19 aastat</b> ja igakuised fondipensioni väljamaksed üle 19 a on <b>tulumaksuvabad.</b></div>
+        <p class="m-0 p-3 pb-2 small text-secondary text-center">Kalkulaator eeldab, et hakkad saama väljamakseid <strong>65-aastaselt</strong> ja <strong>19 aasta jooksul</strong>, sel juhul on sinu igakuised fondipensioni väljamaksed tulumaksuvabad. Aktsiaturu keskmine tootlus on ajalooliselt olnud 7%, kuid see ei taga sarnast tootlust tulevikus.</p>
     </div>
 </div>
