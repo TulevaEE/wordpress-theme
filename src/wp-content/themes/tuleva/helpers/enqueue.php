859a071987d14fe4b14599d100715318517c4dcb
Taimar <tteetlok@gmail.com>
2024-09-30 23:25:27 +0300

    Line-height fix

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -31,7 +31,7 @@ class ThemeEnqueue
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '20240930',
+                'ver' => '20240930.1',
                 'media' => 'all',
                 'enqueue' => true
             ],
