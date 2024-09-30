8bbe54f59a834d3488cecb2b1c36e0f2183a67cf
Taimar <tteetlok@gmail.com>
2024-09-30 23:14:12 +0300

    main.css version update

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -31,7 +31,7 @@ class ThemeEnqueue
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '20240925',
+                'ver' => '20240930',
                 'media' => 'all',
                 'enqueue' => true
             ],
