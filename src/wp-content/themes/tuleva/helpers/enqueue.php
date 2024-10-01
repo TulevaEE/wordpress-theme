4e75581ed83938044dd4b4c713e3085d5d048246
Taimar <tteetlok@gmail.com>
2024-10-01 21:12:10 +0300

    Replaced the return rate line with an arrow

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -31,7 +31,7 @@ class ThemeEnqueue
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '2024.10.01.4',
+                'ver' => '2024.10.01.5',
                 'media' => 'all',
                 'enqueue' => true
             ],
