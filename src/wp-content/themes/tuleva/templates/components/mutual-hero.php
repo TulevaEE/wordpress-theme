0f8b94194610a58861dd2b279c3baa2c6bb94b80
Taimar <tteetlok@gmail.com>
2024-10-02 12:37:29 +0300

    Moved margin to another element for title-only case

--- src/wp-content/themes/tuleva/templates/components/mutual-hero.php
+++ src/wp-content/themes/tuleva/templates/components/mutual-hero.php
@@ -2,10 +2,10 @@
     <div class="container">
         <div class="row py-6">
             <div class="col-md-10 mx-auto text-center">
-                <h1 class="mb-5"><?php the_sub_field('heading'); ?></h1>
+                <h1 class="m-0"><?php the_sub_field('heading'); ?></h1>
             </div>
             <?php if (get_sub_field('quote')) { ?>
-                <div class="col-md-8 mx-auto text-center">
+                <div class="col-md-8 mt-5 mx-auto text-center">
                     <p class="lead text-navy mb-3">"<?php the_sub_field('quote'); ?>"</p>
                     <?php if (get_sub_field('source')) { ?>
                         <p class="text-navy mb-5">
