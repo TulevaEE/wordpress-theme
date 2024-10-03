a8dbb3d7c12bc7c8209a7b0ac974bf5b95be71e5
Taimar <tteetlok@gmail.com>
2024-10-03 15:03:46 +0300

    New color variables; tweaked blog UI elements

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -31,7 +31,7 @@ class ThemeEnqueue
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '2024.10.03.3',
+                'ver' => '2024.10.03.5',
                 'media' => 'all',
                 'enqueue' => true
             ],
