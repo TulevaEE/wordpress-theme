d78bc1a79a8c9b9f102175b90bbfdba2ec9033a4
Taimar <tteetlok@gmail.com>
2024-10-07 16:01:10 +0300

    Visual updates to header and desktop navbar

--- src/wp-content/themes/tuleva/templates/header/main-sticky.php
+++ src/wp-content/themes/tuleva/templates/header/main-sticky.php
@@ -1,6 +1,6 @@
 <header class="header header--sticky">
     <nav class="navbar navbar-expand-md">
-        <a class="navbar-brand mr-lg-6" href="<?php bloginfo('url') ?>">
+        <a class="navbar-brand" href="<?php bloginfo('url') ?>">
             <img class="brand-logo" alt="Tuleva" src="<?php echo get_template_directory_uri() ?>/img/tuleva-logo.svg">
         </a>
         <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNavDropdownSticky" aria-controls="navbarNavDropdownSticky"
