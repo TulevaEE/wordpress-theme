51354e7099ffdcdd409d869e99e429e9a0510371
Taimar <tteetlok@gmail.com>
2024-10-07 16:13:42 +0300

    Removed outdated browser message served to IE8 and older

--- src/wp-content/themes/tuleva/header.php
+++ src/wp-content/themes/tuleva/header.php
@@ -5,9 +5,5 @@
 
 <body <?php body_class(''); ?>>
 
-<!--[if lt IE 9]>
-<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> or <a href="https://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-<![endif]-->
-
 <?php get_template_part('templates/header/main'); ?>
 <?php get_template_part('templates/header/main-sticky'); ?>
