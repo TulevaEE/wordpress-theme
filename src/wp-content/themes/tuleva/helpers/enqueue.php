316b5223b9052539b2068f74b2114074cff6988e
Taimar <tteetlok@gmail.com>
2024-10-10 20:02:05 +0300

    Further visual tweaks

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -31,7 +31,7 @@ class ThemeEnqueue
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '2024.10.10.12',
+                'ver' => '2024.10.10.13',
                 'media' => 'all',
                 'enqueue' => true
             ],
