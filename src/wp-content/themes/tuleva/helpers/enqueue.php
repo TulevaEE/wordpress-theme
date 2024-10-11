36ba08f28e38f088a87dba09d83cdf9ca8c74d61
Taimar <tteetlok@gmail.com>
2024-10-11 17:13:01 +0300

    Typography and layout tweaks

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -31,7 +31,7 @@ class ThemeEnqueue
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '2024.10.11.1',
+                'ver' => '2024.10.11.2',
                 'media' => 'all',
                 'enqueue' => true
             ],
--- src/wp-content/themes/tuleva/templates/components/tuleva-difference.php
+++ src/wp-content/themes/tuleva/templates/components/tuleva-difference.php
@@ -1,21 +1,25 @@
 <section id="<?php the_sub_field('component_id'); ?>" class="py-6">
-<div class="container">
-  <div class="row text-center">
-    <div class="col-md-10 m-auto">
-      <h2 class="mb-4"><?php the_sub_field('heading'); ?></h2>
-      <p class="lead"><?php the_sub_field('lead_text'); ?></p>
-      <img src="<?php the_sub_field('image'); ?>" alt="" class="img-fluid mb-5">
+  <div class="container">
+    <div class="row text-center">
+      <div class="mx-auto col-lg-11 col-xl-9">
+        <h2><?php the_sub_field('heading'); ?></h2>
+        <p class="lead m-0"><?php the_sub_field('lead_text'); ?></p>
+      </div>
     </div>
-  </div>
-  <div class="row">
-    <div class="mx-auto col-lg-9 col-xl-8">
-      <div class="mb-5">
+    <div class="row py-5 text-center">
+      <div class="mx-auto col-lg-11 col-xl-9">
+        <img src="<?php the_sub_field('image'); ?>" alt="" class="img-fluid">
+      </div>
+    </div>
+    <div class="row">
+      <div class="mx-auto col-md-11 col-lg-8 col-xl-7">
+        <div>
           <?php echo get_sub_field('text'); ?>
-      </div>
-      <div class="text-center">
-        <a href="<?php echo get_sub_field('button_url'); ?>" class="btn btn-outline-primary btn-lg"><?php the_sub_field('button_text'); ?></a>
+        </div>
+        <div class="mt-5 text-center">
+          <a href="<?php echo get_sub_field('button_url'); ?>" class="btn btn-outline-primary btn-lg"><?php the_sub_field('button_text'); ?></a>
+        </div>
       </div>
     </div>
   </div>
-</div>
 </section>
