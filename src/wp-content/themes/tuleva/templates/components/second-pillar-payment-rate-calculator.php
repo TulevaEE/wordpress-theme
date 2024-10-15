99067f6a2aabbf3198bc5bda51b65dfccae70086
Erko Risthein <erko@risthein.ee>
2024-10-15 10:30:58 +0300

    Update the 2nd pillar payment rate calculator copy

--- src/wp-content/themes/tuleva/templates/components/second-pillar-payment-rate-calculator.php
+++ src/wp-content/themes/tuleva/templates/components/second-pillar-payment-rate-calculator.php
@@ -1,7 +1,7 @@
 <div class="card shadow-md br-3">
     <div class="card-body p-2 second-pillar-payment-rate-calculator">
         <h2 class="h5 m-0 mb-2 py-3 text-navy text-center">
-            <?php _e('What does increasing your II pillar payments mean to you?', TEXT_DOMAIN); ?>
+            <?php _e('What does increasing II pillar payments mean for you?', TEXT_DOMAIN); ?>
         </h2>
         <div class="card bg-blue-washed p-3 br-2">
             <div class="form-group row">
