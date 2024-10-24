2b96c66967be8be67f46a87a449e1fa68c35808f
Taimar <tteetlok@gmail.com>
2024-10-24 19:32:03 +0300

    Slightly smaller paddings for footnote links

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -31,7 +31,7 @@ class ThemeEnqueue
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '2024.10.24.2',
+                'ver' => '2024.10.24.3',
                 'media' => 'all',
                 'enqueue' => true
             ],
