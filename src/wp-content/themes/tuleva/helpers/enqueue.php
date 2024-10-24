3d08eb642c9aae3ced9986a2544f0908280a3372
Taimar <tteetlok@gmail.com>
2024-10-24 13:40:58 +0300

    Better footnotes and other tweaks

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -31,7 +31,7 @@ class ThemeEnqueue
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '2024.10.17.5',
+                'ver' => '2024.10.24.1',
                 'media' => 'all',
                 'enqueue' => true
             ],
