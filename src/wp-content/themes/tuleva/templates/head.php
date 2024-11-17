bca157608ab1e43df30eecb2a5c5df4505448978
Jordan Valdma <deskpro_belchertown@simplelogin.com>
2024-11-17 13:59:39 +0700

    Prevent iOS device zoom

--- src/wp-content/themes/tuleva/templates/head.php
+++ src/wp-content/themes/tuleva/templates/head.php
@@ -4,4 +4,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php get_template_part('templates/head/analytics'); ?>
     <?php wp_head(); ?>
+    <script>
+
+    <!-- Temporary fix for iOS zooming crash -->
+    if (/iPhone|iPad|iPod/.test(navigator.userAgent)) {
+        const handleTouchEvent = (event) => {
+            if (event.type.startsWith("gesture")) {
+                event.preventDefault();
+            }
+        };
+
+        const attachTouchListeners = () => {
+            document.addEventListener('gesturestart', handleTouchEvent, { passive: false });
+            document.addEventListener('gesturechange', handleTouchEvent, { passive: false });
+            document.addEventListener('gestureend', handleTouchEvent, { passive: false });
+            console.log("attached iOS touch event listeners");
+        };
+
+        attachTouchListeners();
+    }
+    </script>
 </head>
