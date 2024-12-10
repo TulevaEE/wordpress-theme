50dd2a329e0de00452c295fd2afae09c9a0d2f8e
Taimar <tteetlok@gmail.com>
2024-12-10 19:41:27 +0200

    Enqueue fix

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -31,7 +31,7 @@ class ThemeEnqueue
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '2024.12.05.1',
+                'ver' => '2024.12.10.1',
                 'media' => 'all',
                 'enqueue' => true
             ],
