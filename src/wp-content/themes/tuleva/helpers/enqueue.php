f876356f6ab251f44cba64c1268fc709c1fc4a5d
Taimar <tteetlok@gmail.com>
2025-02-14 13:23:33 +0700

    One more tweak

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -31,7 +31,7 @@ class ThemeEnqueue
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '2025.02.14.1',
+                'ver' => '2025.02.14.2',
                 'media' => 'all',
                 'enqueue' => true
             ],
