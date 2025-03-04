4e8f52a2227e5550664345734b3efcb03e24e4d3
Taimar <tteetlok@gmail.com>
2025-03-04 19:52:04 +0700

    Update copy and layout

--- src/wp-content/themes/tuleva/templates/components/fund-bonds-header.php
+++ src/wp-content/themes/tuleva/templates/components/fund-bonds-header.php
@@ -2,8 +2,7 @@
     <div class="container">
         <div class="row py-6">
             <div class="col-lg-10 mx-auto text-center">
-                <h1 class="mb-4"><?php _e('Tuleva World Bonds Pension Fund', TEXT_DOMAIN) ?></h1>
-                <p class="lead mb-4"><?php _e('Conservative index fund that invests fully in bonds.', TEXT_DOMAIN) ?></p>
+                <h1 class="mb-5"><?php _e('Tuleva World Bonds Pension Fund', TEXT_DOMAIN) ?></h1>
             </div>
         </div>
     </div>
@@ -15,11 +14,8 @@
             <div class="col-lg-10 m-auto">
                 <div class="proposal">
                     <div class="proposal__body">
-                        <p class="mb-3 lead fw-bold"><?php _e('Suitable for you if:', TEXT_DOMAIN) ?></p>
-                        <ul class="list-style-checkmark">
-                            <li><?php _e('You plan to withdraw your entire II pillar savings within the next 5 years.', TEXT_DOMAIN) ?></li>
-                            <li><?php _e('You want protection against market fluctuations and are willing to give up returns for it.', TEXT_DOMAIN) ?></li>
-                        </ul>
+                        <p class="mb-3"><?php _e('<strong>A conservative index fund that invests entirely in low-risk bonds.</strong> The expected return of this fund is significantly lower than that of the stock fund. It is impossible to achieve good returns with low risk.', TEXT_DOMAIN) ?></p>
+                        <p class="m-0"><?php _e('Suitable for you if you plan to withdraw your entire II pillar within the next 5 years and are willing to give up returns for security.', TEXT_DOMAIN) ?></p>
                     </div>
                     <div class="proposal__cta">
                         <?php _e('<a href="/en/online-bank-instructions/" class="btn btn-primary btn-lg btn-block ga" data-label="fund-details-choose-bonds">Select this fund</a>', TEXT_DOMAIN); ?>
--- src/wp-content/themes/tuleva/templates/components/fund-third-header.php
+++ src/wp-content/themes/tuleva/templates/components/fund-third-header.php
@@ -2,8 +2,7 @@
     <div class="container">
         <div class="row py-6">
             <div class="col-lg-10 mx-auto text-center">
-                <h1 class="mb-4"><?php _e('Tuleva III Pillar Pension Fund', TEXT_DOMAIN) ?></h1>
-                <p class="lead mb-4"><?php _e('Low-cost index fund that invests fully in stocks.', TEXT_DOMAIN) ?></p>
+                <h1 class="mb-5"><?php _e('Tuleva III Pillar Pension Fund', TEXT_DOMAIN) ?></h1>
             </div>
         </div>
     </div>
@@ -15,11 +14,8 @@
             <div class="col-lg-10 m-auto">
                 <div class="proposal">
                     <div class="proposal__body">
-                        <p class="mb-3 lead fw-bold"><?php _e('Suitable for you if:', TEXT_DOMAIN); ?></p>
-                        <ul class="list-style-checkmark">
-                            <li><?php _e('You seek the best long‑term returns and are not unsettled by short‑term market fluctuations.', TEXT_DOMAIN); ?></li>
-                            <li><?php _e('You do not plan to withdraw your entire III pillar within the next 5 years.', TEXT_DOMAIN); ?></li>
-                        </ul>
+                        <p class="mb-3"><?php _e('<strong>A low-cost index fund that invests entirely in stocks.</strong> The performance of this fund can fluctuate sharply from year to year. However, the expected return is significantly higher than that of the bond fund.', TEXT_DOMAIN) ?></p>
+                        <p class="m-0"><?php _e('Suitable for you if you want to achieve <strong>the best long-term returns</strong> and do not plan to withdraw your entire III pillar within the next 5 years.', TEXT_DOMAIN) ?></p>
                     </div>
                     <div class="proposal__cta">
                         <a href="<?php echo get_site_url(); ?>/iii-sammas-juhend" class="btn btn-primary btn-lg btn-block ga" data-label="fund-details-choose-stocks"><?php _e('Select this fund', TEXT_DOMAIN); ?></a>
