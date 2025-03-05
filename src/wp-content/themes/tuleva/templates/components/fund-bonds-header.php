3b4e2d806325ce8d617f659689e9be255e7ac4a4
Taimar <tteetlok@gmail.com>
2025-03-05 18:30:45 +0700

    Unified copy; CSS tweaks

--- src/wp-content/themes/tuleva/templates/components/fund-bonds-header.php
+++ src/wp-content/themes/tuleva/templates/components/fund-bonds-header.php
@@ -14,8 +14,8 @@
             <div class="col-lg-10 m-auto">
                 <div class="proposal">
                     <div class="proposal__body">
-                        <p class="mb-3"><?php _e('<strong>A conservative index fund that invests entirely in low-risk bonds.</strong> The expected return of this fund is significantly lower than that of the stock fund. It is impossible to achieve good returns with low risk.', TEXT_DOMAIN) ?></p>
-                        <p class="m-0"><?php _e('Suitable for you if you plan to withdraw your entire II pillar within the next 5 years and are willing to give up returns for security.', TEXT_DOMAIN) ?></p>
+                        <p class="mb-3"><?php _e('<strong>A conservative index fund that invests entirely in low-risk bonds.</strong> Lower risk also means lower returns compared to the stock fund. It is impossible to combine low risk with high returns.', TEXT_DOMAIN) ?></p>
+                        <p class="m-0"><?php _e('Suitable for you if you plan to withdraw your entire II pillar within the next 5 years and are willing to give up returns for security.', TEXT_DOMAIN) ?></p>
                     </div>
                     <div class="proposal__cta">
                         <?php _e('<a href="/en/online-bank-instructions/" class="btn btn-primary btn-lg btn-block ga" data-label="fund-details-choose-bonds">Select this fund</a>', TEXT_DOMAIN); ?>
