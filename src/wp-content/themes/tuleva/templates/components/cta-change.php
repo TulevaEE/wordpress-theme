6ca117d9ee02aafa76aa566296432b524fe17c15
Taimar <tteetlok@gmail.com>
2025-03-10 12:29:51 +0700

    Visual and markup improvements to Change CTA component

--- src/wp-content/themes/tuleva/templates/components/cta-change.php
+++ src/wp-content/themes/tuleva/templates/components/cta-change.php
@@ -1,8 +1,8 @@
 <section id="<?php the_sub_field('component_id'); ?>" class="py-5 cta-change<?php echo get_sub_field('background_color') === 'blue' ? ' bg-blue-washed' : ''; ?>">
     <div class="container">
-      <div class="row text-center">
-        <div class="d-flex flex-column flex-md-row w-100 justify-content-center align-items-center">
-          <h5 class="mb-md-0 mr-md-3">
+      <div class="row">
+        <p class="col-12 d-flex flex-column flex-md-row justify-content-center align-items-md-baseline m-0 text-center">
+          <span class="mb-4 mb-md-0 mr-md-4 fs-5">
               <?php
               $isMarch = (date('m') == 3);
               $isJuly = (date('m') == 7);
@@ -19,9 +19,9 @@
                   echo get_sub_field('text');
               }
               ?>
-          </h5>
+          </span>
           <a href="<?php echo get_sub_field('button_url'); ?>" class="btn btn-primary btn-lg"><?php the_sub_field('button_text'); ?></a>
-        </div>
+        </p>
       </div>
     </div>
 </section>
