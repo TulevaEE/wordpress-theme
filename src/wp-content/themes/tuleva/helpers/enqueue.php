05ccee90aea21bc6722cde9cf9a982bf0e2271aa
Taimar <tteetlok@gmail.com>
2025-03-31 23:11:40 +0700

    Change selector for scroll-margin-top

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -31,7 +31,7 @@ class ThemeEnqueue
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '2025.03.31-6',
+                'ver' => '2025.03.31-7',
                 'media' => 'all',
                 'enqueue' => true
             ],
