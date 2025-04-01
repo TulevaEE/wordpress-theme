bc81585e6baf5836a55a326684603e6370bcf081
Taimar <tteetlok@gmail.com>
2025-04-01 21:57:44 +0700

    Remove temporary CSS rules

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -31,7 +31,7 @@ class ThemeEnqueue
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '2025.04.01-2',
+                'ver' => '2025.04.01-3',
                 'media' => 'all',
                 'enqueue' => true
             ],
