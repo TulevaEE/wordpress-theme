d0ea65b52df17a45981e7f7fb87aec74202d8343
Taimar <tteetlok@gmail.com>
2025-04-01 12:00:07 +0700

    Make quotes layout nicer on mobile

--- src/wp-content/themes/tuleva/templates/components/story/quote.php
+++ src/wp-content/themes/tuleva/templates/components/story/quote.php
@@ -1,5 +1,5 @@
-<div class="row align-items-center py-4">
-    <div class="offset-md-2 col-md-5 col-sm-7">
+<div class="row align-items-center py-4 text-center text-md-left">
+    <div class="offset-md-2 col-md-5 col-sm-7 mb-3 mb-md-0">
         <?php echo get_sub_field('text'); ?>
     </div>
     <div class="col-md-3 col-sm-3 text-center">
