fbe3b1e210f66bff8550c4aab8186052747f8104
Taimar <tteetlok@gmail.com>
2025-04-02 18:06:39 +0700

    Fix hero section height on small viewport heights

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -31,7 +31,7 @@ class ThemeEnqueue
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '2025.04.01-3',
+                'ver' => '2025.04.02',
                 'media' => 'all',
                 'enqueue' => true
             ],
