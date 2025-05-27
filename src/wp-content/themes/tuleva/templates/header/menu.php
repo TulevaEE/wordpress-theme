f6a737e716afbcb8fc9127622f3aa9b5c5670d80
Erko Risthein <erko@risthein.ee>
2025-05-27 16:25:52 +0300

    Fix menu class

--- src/wp-content/themes/tuleva/templates/header/menu.php
+++ src/wp-content/themes/tuleva/templates/header/menu.php
@@ -3,7 +3,7 @@ if (has_nav_menu('primary_navigation')) {
     $args = [
         'theme_location'  => 'primary_navigation',
         'container'       => false,
-        'menu_class'      => 'nav navbar-nav ml-auto',
+        'menu_class'      => 'nav navbar-nav ms-auto',
         'echo'            => true,
         'fallback_cb'     => 'wp_page_menu',
         'depth'           => 2,
