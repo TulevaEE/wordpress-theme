56080bed4b7522b9cdef5af36cd9e4a8458a5ff2
Taimar <tteetlok@gmail.com>
2025-05-28 21:25:06 +0300

    Replace H2 with H1 in hero; add extra padding

--- src/wp-content/themes/tuleva/templates/components/fees-hero.php
+++ src/wp-content/themes/tuleva/templates/components/fees-hero.php
@@ -1,9 +1,9 @@
 <section id="<?php the_sub_field('component_id'); ?>" class="bg-hero-fees">
     <div class="container">
-         <div class="row py-5">
-            <div class="col-lg-8 mx-auto text-center">
-                <h2><span class="text-highlight"><?php the_sub_field('heading'); ?></span></h2>
-                <p class="lead text-navy mb-0"><?php the_sub_field('lead_text'); ?></p>
+         <div class="row py-6">
+            <div class="col-lg-8 mx-auto text-center text-navy">
+                <h1 class="m-0"><span class="text-highlight"><?php the_sub_field('heading'); ?></span></h1>
+                <p class="m-0 mt-5 lead"><?php the_sub_field('lead_text'); ?></p>
             </div>
         </div>
     </div>
