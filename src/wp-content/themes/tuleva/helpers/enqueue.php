a3790bf0e8a8c037f67d31fb52b05fa5f1b0a19c
Taimar <tteetlok@gmail.com>
2025-05-29 16:31:19 +0300

    Improve historic-return-rate hint positioning

--- src/wp-content/themes/tuleva/helpers/enqueue.php
+++ src/wp-content/themes/tuleva/helpers/enqueue.php
@@ -31,7 +31,7 @@ class ThemeEnqueue
                 'handle' => 'main-style',
                 'src' => $css_path . 'main.css',
                 'deps' => 'normalize',
-                'ver' => '2025.05.29.5',
+                'ver' => '2025.05.29.6',
                 'media' => 'all',
                 'enqueue' => true
             ],
