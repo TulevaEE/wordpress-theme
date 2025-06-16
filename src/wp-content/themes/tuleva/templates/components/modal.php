5617ace81b4a3cebd211ffd4cef50ca71b65010b
Taimar <tteetlok@gmail.com>
2025-06-16 15:37:02 +0300

    Make modals not reachable via sequential keyboard navigation

--- src/wp-content/themes/tuleva/templates/components/modal.php
+++ src/wp-content/themes/tuleva/templates/components/modal.php
@@ -1,6 +1,6 @@
 <?php if( have_rows('generic_modals', 'option') ): ?>
     <?php while( have_rows('generic_modals', 'option') ): the_row(); ?>
-        <div id="modal-<?php the_sub_field('modal_id'); ?>" class="modal-full">
+        <div id="modal-<?php the_sub_field('modal_id'); ?>" tabindex="-1" class="modal-full">
             <div class="close-button-modal-<?php the_sub_field('modal_id'); ?>">
                 <img src="<?php echo get_template_directory_uri() ?>/img/icon-close.svg" alt="Close">
             </div>
