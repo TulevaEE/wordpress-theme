b7e4bded6d2d3589d724ee7306fa126496ae8f83
Taimar <tteetlok@gmail.com>
2025-06-17 11:58:27 +0300

    Improve search form and its labels

--- src/wp-content/themes/tuleva/searchform.php
+++ src/wp-content/themes/tuleva/searchform.php
@@ -1,9 +1,7 @@
-<form role="search" method="get" class="search-form d-flex align-items-center" action="<?php echo home_url('/'); ?>">
-    <label class="mb-0 me-1">
-        <span class="screen-reader-text"><?php echo _x('Search for:', ''); ?></span>
-        <input type="search" class="search-field form-control" placeholder="<?php echo esc_attr_x('Search', ''); ?>…" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x('Search for:', 'label'); ?>" />
-    </label>
+<form role="search" method="get" class="search-form d-flex align-items-center gap-2" action="<?php echo home_url('/'); ?>">
+    <label for="searchBlog" class="form-label visually-hidden"><?php _e('Search blog', TEXT_DOMAIN) ?></label>
+    <input type="search" class="search-field form-control flex-grow-1" value="<?php echo get_search_query(); ?>" name="s" id="searchBlog" />
     <button type="submit" class="search-submit btn btn-outline-primary">
-        <span class="icon-search"><?php echo esc_html_x('Search', ''); ?></span>
+        <?php echo esc_html_x('Search', ''); ?>
     </button>
 </form>
