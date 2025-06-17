5d4cf08e5874fdbe43f452ce79e15010f30c52fd
Taimar <tteetlok@gmail.com>
2025-06-17 13:57:36 +0300

    Update prospectus links

--- src/wp-content/themes/tuleva/templates/components/fund-stocks-details.php
+++ src/wp-content/themes/tuleva/templates/components/fund-stocks-details.php
@@ -54,7 +54,7 @@
                         <h5 class="mb-4"><?php _e('Documents', TEXT_DOMAIN) ?></h5>
                         <ul class="list-style-arrow mb-5">
                             <li>
-                                <a href="<?php echo get_site_url(); ?>/wp-content/uploads/2025/06/TUK75-ja-TUK00-Prospekt-kehtib-alates-03.03.2025.pdf" target="_blank"><?php _e('Prospectus', TEXT_DOMAIN) ?></a><?php _e(' and ', TEXT_DOMAIN) ?><a href="<?php echo get_site_url(); ?>/wp-content/uploads/2022/09/Tuleva-Maailma-Aktsiate-Pensionifondi-tingimused_01.09.2022-1.pdf" target="_blank"><?php _e('Terms and conditions', TEXT_DOMAIN) ?></a><?php _e(' (in Estonian)', TEXT_DOMAIN) ?>
+                                <a href="<?php echo get_site_url(); ?>/wp-content/uploads/2025/06/TUK75-ja-TUK00-Prospekt-kehtib-alates-04.03.2025.pdf" target="_blank"><?php _e('Prospectus', TEXT_DOMAIN) ?></a><?php _e(' and ', TEXT_DOMAIN) ?><a href="<?php echo get_site_url(); ?>/wp-content/uploads/2022/09/Tuleva-Maailma-Aktsiate-Pensionifondi-tingimused_01.09.2022-1.pdf" target="_blank"><?php _e('Terms and conditions', TEXT_DOMAIN) ?></a><?php _e(' (in Estonian)', TEXT_DOMAIN) ?>
                                 <br />
                                 <?php _e('<a href="https://tuleva.ee/en/analysis/we-made-the-terms-of-our-pension-funds-more-precise/">Change of terms from 01.09.2025</a>', TEXT_DOMAIN) ?>
                             </li>
