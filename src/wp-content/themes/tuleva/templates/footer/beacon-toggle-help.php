424ac075db3f8ff6614fd6286b25f36e3e55b22e
Taimar <tteetlok@gmail.com>
2025-06-17 18:20:00 +0300

    Make beacon-toggle more accessible

--- src/wp-content/themes/tuleva/templates/footer/beacon-toggle-help.php
+++ src/wp-content/themes/tuleva/templates/footer/beacon-toggle-help.php
@@ -1,4 +1,4 @@
-<a href="#" class="beacon-toggle beacon-toggle--help">
+<a role="button" tabindex="0" href="#" class="beacon-toggle beacon-toggle--help focus-ring">
     <span class="beacon-toggle__icon"></span>
     <span class="beacon-toggle__text"><?php _e('I have a question', TEXT_DOMAIN) ?></span>
 </a>
