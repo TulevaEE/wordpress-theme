a74f35eac1760842754fe083b79a8a1321173ef9
Taimar <tteetlok@gmail.com>
2025-06-17 14:58:15 +0300

    Add alt-text support for media-block template

--- src/wp-content/themes/tuleva/templates/components/media-block/media-block-image.php
+++ src/wp-content/themes/tuleva/templates/components/media-block/media-block-image.php
@@ -5,6 +5,7 @@ $image_url = wp_get_attachment_image_url($image_id, 'large');
 $image_srcset = wp_get_attachment_image_srcset($image_id,'large');
 $image_link_url = get_sub_field('image_link_url');
 $image_icon_url = get_sub_field('image_icon');
+$image_alt = get_post_meta($image_id, '_wp_attachment_image_alt', TRUE);
 $image_caption = wp_get_attachment_caption($image_id);
 ?>
 
@@ -13,7 +14,7 @@ $image_caption = wp_get_attachment_caption($image_id);
         <?php if ($image_link_url) { ?>
             <a href="<?php echo $image_link_url; ?>">
         <?php } ?>
-            <img class="img-fluid" src="<?php echo $image_url; ?>" srcset="<?php echo $image_srcset; ?>" alt="<?php the_sub_field('heading') ?>">
+            <img class="img-fluid" src="<?php echo $image_url; ?>" srcset="<?php echo $image_srcset; ?>" alt="<?php echo esc_attr($image_alt); ?>">
             <?php if ($image_icon_url) { ?>
                 <svg class="media-block__media__icon">
                     <image xlink:href="<?php echo $image_icon_url; ?>"/>
