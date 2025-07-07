ba67678ec7dd2c8ab15d17f2dc8c64a2021b3f19
Taimar <tteetlok@gmail.com>
2025-07-07 16:37:10 +0300

    Remove duplicate image columns and visibility hacks; use reverse direction instead

--- src/wp-content/themes/tuleva/templates/components/media-block/media-block.php
+++ src/wp-content/themes/tuleva/templates/components/media-block/media-block.php
@@ -1,35 +1,36 @@
 <?php
-$bg_class = get_component_background_color_class();
-$image_alignment = strtolower(get_sub_field('image_alignment'));
-$original_image_url = get_sub_field('image');
-$image_id = attachment_url_to_postid($original_image_url);
-$image_url = wp_get_attachment_image_url($image_id, 'large');
-$content_class = $image_url ? 'col-lg-7 col-xl-6' : 'col-lg-12';
-$left_image_visibility_class = $image_url && $image_alignment === 'right' ? 'd-block d-lg-none' : '';
-$right_image_visibility_class = $image_url && $image_alignment === 'right' ? 'd-none d-lg-block' : '';
+$bg_class     = get_component_background_color_class();
+$image_pos    = strtolower( get_sub_field( 'image_alignment' ) );
+$img_url      = wp_get_attachment_image_url(
+                    attachment_url_to_postid( get_sub_field( 'image' ) ),
+                    'large'
+                );
+$has_img      = ! empty( $img_url );
+$content_col  = $has_img
+                ? 'col-lg-7 col-xl-6'
+                : 'col-lg-12';
+$row_dir      = ( $has_img && $image_pos === 'right' )
+                ? 'flex-lg-row-reverse'
+                : '';
 ?>
-<section class="section-media d-flex flex-column section-spacing <?php echo $bg_class; ?>">
-    <div class="container my-auto">
-        <div class="row align-items-start align-items-xl-center gy-5 gx-lg-5">
-            <?php if ($image_url) { ?>
-                <div class="section-media-slot col-lg-5 col-xl-6 <?php echo $left_image_visibility_class; ?>">
-                    <?php get_template_part('templates/components/media-block/media-block-image'); ?>
-                </div>
-            <?php } ?>
-
-            <div class="<?php echo $content_class ?> mx-auto">
-                <h2 class="mb-4"><?php the_sub_field('heading') ?></h2>
-                <?php if (get_sub_field('lead_text')): ?>
-                    <p class="lead mb-3"><?php the_sub_field('lead_text') ?></p>
-                <?php endif; ?>
-                <?php echo get_sub_field('text'); ?>
-            </div>
-
-            <?php if ($image_url && $image_alignment === 'right'): ?>
-                <div class="section-media-slot col-lg-5 col-xl-6 <?php echo $right_image_visibility_class; ?>">
-                    <?php get_template_part('templates/components/media-block/media-block-image'); ?>
-                </div>
-            <?php endif; ?>
+<section class="section-media d-flex flex-column section-spacing <?php echo esc_attr( $bg_class ); ?>">
+  <div class="container my-auto">
+    <div class="row align-items-start align-items-xl-center gy-5 gx-xl-5 <?php echo esc_attr( $row_dir ); ?>">
+      <?php if ( $has_img ) : ?>
+        <div class="section-media-slot col-lg-5 col-xl-6">
+          <?php get_template_part( 'templates/components/media-block/media-block-image' ); ?>
         </div>
+      <?php endif; ?>
+
+      <div class="<?php echo esc_attr( $content_col ); ?> mx-auto">
+        <h2 class="mb-4"><?php the_sub_field( 'heading' ); ?></h2>
+
+        <?php if ( get_sub_field( 'lead_text' ) ) : ?>
+          <p class="lead mb-3"><?php the_sub_field( 'lead_text' ); ?></p>
+        <?php endif; ?>
+
+        <?php echo get_sub_field( 'text' ); ?>
+      </div>
     </div>
+  </div>
 </section>
