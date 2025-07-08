a60d9b33389b1281b55d59a3d0091dceeaccaf27
Taimar <tteetlok@gmail.com>
2025-07-08 13:37:08 +0300

    Separate SCSS file for high-contrast styles; more high-contrast styles; linkable footer (added ID)

--- src/wp-content/themes/tuleva/footer.php
+++ src/wp-content/themes/tuleva/footer.php
@@ -1,4 +1,4 @@
-<footer class="footer">
+<footer id="footer" class="footer">
     <?php if (isset($_GET['development'])) : ?>
         <div class="px-3 d-flex align-items-center justify-content-center">
             <?php get_template_part('templates/footer/not-logged-in-mailing-list-form'); ?>
@@ -14,6 +14,12 @@
             <?php if (is_active_sidebar('footer_bottom_widget_area')) : ?>
                 <?php dynamic_sidebar('footer_bottom_widget_area'); ?>
             <?php endif; ?>
+        </div>
+        <div class="row">
+            <div class="form-check form-switch">
+                <input class="form-check-input high-contrast-toggle" type="checkbox" role="switch" id="high-contrast-toggle">
+                <label class="form-check-label" for="high-contrast-toggle">Suurenda kontrasti</label>
+            </div>
         </div>
     </div>
 </footer>
