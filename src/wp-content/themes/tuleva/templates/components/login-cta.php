ee5e57606ed35ffb5c98ed9dcbfe79dc8c88475e
Taimar <tteetlok@gmail.com>
2025-07-22 13:37:31 +0300

    Improve login-cta layout and markup

--- src/wp-content/themes/tuleva/templates/components/login-cta.php
+++ src/wp-content/themes/tuleva/templates/components/login-cta.php
@@ -4,7 +4,7 @@ $bg_class = get_component_background_color_class();
 <section id="<?php the_sub_field('component_id'); ?>" class="d-flex flex-column section-spacing <?php echo $bg_class; ?>">
     <div class="container my-auto">
         <div class="row align-items-center gx-xl-5">
-            <div class="col-lg-12 text-center text-lg-start">
+            <div class="col-lg-12">
                 <h1><?php the_sub_field('heading'); ?></h1>
             </div>
             <div class="col-lg-6">
@@ -15,25 +15,19 @@ $bg_class = get_component_background_color_class();
             </div>
             <?php if (get_sub_field('button_text') && get_sub_field('button_url')): ?>
                 <div class="col-lg-6">
-                    <div class="d-flex justify-content-center mb-2">
-                        <div class="p-2">
-                            <img src="<?php echo get_template_directory_uri() ?>/img/id-card-logo.png" style="max-height:32px;" class="img-fluid" alt="<?php esc_attr_e( 'ID-card', '' ); ?>">
-                        </div>
-                        <div class="p-2">
-                            <img src="<?php echo get_template_directory_uri() ?>/img/mobile-id-logo.png" style="max-height:32px;" class="img-fluid" alt="<?php esc_attr_e( 'Mobile-ID', '' ); ?>">
-                        </div>
-                        <div class="p-2">
-                            <img src="<?php echo get_template_directory_uri() ?>/img/smart-id-logo.png" style="max-height: 32px;" class="img-fluid" alt="<?php esc_attr_e( 'Smart-ID', '' ); ?>">
-                        </div>
-                    </div>
-                    <div class="text-center">
-                        <a href="<?php echo get_sub_field('button_url'); ?>" class="btn btn-primary btn-lg px-6 mb-3"><?php the_sub_field('button_text'); ?></a>
+                    <p class="d-flex justify-content-center mt-5 mt-lg-0 mb-4 gap-4">
+                        <img src="<?php echo get_template_directory_uri() ?>/img/id-card-logo.png" style="max-height:32px;" class="img-fluid" alt="<?php esc_attr_e( 'ID-card', '' ); ?>">
+                        <img src="<?php echo get_template_directory_uri() ?>/img/mobile-id-logo.png" style="max-height:32px;" class="img-fluid" alt="<?php esc_attr_e( 'Mobile-ID', '' ); ?>">
+                        <img src="<?php echo get_template_directory_uri() ?>/img/smart-id-logo.png" style="max-height: 32px;" class="img-fluid" alt="<?php esc_attr_e( 'Smart-ID', '' ); ?>">
+                    </p>
+                    <p class="d-flex flex-column m-0 text-center">
+                        <a href="<?php echo get_sub_field('button_url'); ?>" class="btn btn-primary btn-lg mb-4"><?php the_sub_field('button_text'); ?></a>
                         <?php if (get_sub_field('small_text')): ?>
-                            <p class="small text-navy mb-md-5 mb-lg-0">
+                            <span class="small text-navy">
                                 <?php the_sub_field('small_text'); ?>
-                            </p>
+                            </span>
                         <?php endif; ?>
-                    </div>
+                    </p>
                 </div>
             <?php endif; ?>
         </div>
