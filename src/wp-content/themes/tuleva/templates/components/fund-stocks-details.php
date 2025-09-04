a7ced41e3028b1bab086d4601a5316f1bd27be24
taavipertman-glitch <taavi.pertman@tuleva.ee>
2025-09-04 14:42:17 +0300

    Update fund-stocks-details.php

--- src/wp-content/themes/tuleva/templates/components/fund-stocks-details.php
+++ src/wp-content/themes/tuleva/templates/components/fund-stocks-details.php
@@ -79,7 +79,7 @@
                         <h2 class="mt-5 mb-4 h4"><?php _e('Reports', TEXT_DOMAIN) ?></h2>
                         <ul class="list-style-arrow mb-5 text-secondary">
                             <li>
-                                <?php echo generate_report_link('https://tuleva.ee/wp-content/uploads/2025/08/Tuleva-Maailma-Aktsiate-Pensionifondi-investeeringute-aruanne-juuli-2025.pdf',__('Investment reports', TEXT_DOMAIN)); ?><?php _e(' (in Estonian)', TEXT_DOMAIN) ?>
+                                <?php echo generate_report_link('https://tuleva.ee/wp-content/uploads/2025/09/Tuleva-Maailma-Aktsiate-Pensionifondi-investeeringute-aruanne-august-2025.pdf',__('Investment reports', TEXT_DOMAIN)); ?><?php _e(' (in Estonian)', TEXT_DOMAIN) ?>
                                 <br>
                                 <a href="https://www.pensionikeskus.ee/ii-sammas/kohustuslikud-pensionifondid/fid/77/" target="_blank"><?php _e('Previous reports', TEXT_DOMAIN) ?></a>
                             </li>
