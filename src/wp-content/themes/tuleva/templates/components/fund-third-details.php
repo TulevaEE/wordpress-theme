b28adbf006e0eb374e3bd2f33c8d1d83f269282d
taavipertman-glitch <taavi.pertman@tuleva.ee>
2026-02-06 10:16:54 +0200

    Update fund-third-details.php
    investeeringute aruanna

--- src/wp-content/themes/tuleva/templates/components/fund-third-details.php
+++ src/wp-content/themes/tuleva/templates/components/fund-third-details.php
@@ -82,7 +82,7 @@
                         <h2 class="mt-5 mb-4 h4"><?php _e('Reports', TEXT_DOMAIN) ?></h2>
                         <ul class="list-style-arrow text-secondary">
                             <li>
-                                <?php echo generate_report_link('https://tuleva.ee/wp-content/uploads/2026/01/Tuleva-III-Samba-Pensionifondi-investeeringute-aruanne-detsember-2025.pdf',__('Investment reports', TEXT_DOMAIN)); ?><?php _e(' (in Estonian)', TEXT_DOMAIN) ?>
+                                <?php echo generate_report_link('https://tuleva.ee/wp-content/uploads/2026/02/Tuleva-III-Samba-Pensionifondi-investeeringute-aruanne-jaanuar-2026.pdf',__('Investment reports', TEXT_DOMAIN)); ?><?php _e(' (in Estonian)', TEXT_DOMAIN) ?>
                                 <br>
                                 <a href="https://www.pensionikeskus.ee/iii-sammas/vabatahtlikud-fondid/fid/81/" target="_blank"><?php _e('Previous reports', TEXT_DOMAIN) ?></a>
                             </li>
