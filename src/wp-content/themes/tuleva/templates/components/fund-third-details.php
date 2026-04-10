18587516bf70b769917386dee16f841802017d3e
taavipertman-glitch <taavi.pertman@tuleva.ee>
2026-04-10 09:43:39 +0300

    Update investment report link for March 2026 (#60)

--- src/wp-content/themes/tuleva/templates/components/fund-third-details.php
+++ src/wp-content/themes/tuleva/templates/components/fund-third-details.php
@@ -79,7 +79,7 @@
                         <h2 class="mt-5 mb-4 h4"><?php _e('Reports', TEXT_DOMAIN) ?></h2>
                         <ul class="list-style-arrow text-secondary">
                             <li>
-                                <?php echo generate_report_link('https://tuleva.ee/wp-content/uploads/2026/03/Tuleva-III-Samba-Pensionifondi-investeeringute-aruanne-veebruar-2026.pdf',__('Investment reports', TEXT_DOMAIN)); ?><?php _e(' (in Estonian)', TEXT_DOMAIN) ?>
+                                <?php echo generate_report_link('https://tuleva.ee/wp-content/uploads/2026/04/Tuleva-III-Samba-Pensionifondi-investeeringute-aruanne-marts-2026.pdf',__('Investment reports', TEXT_DOMAIN)); ?><?php _e(' (in Estonian)', TEXT_DOMAIN) ?>
                                 <br>
                                 <a href="https://www.pensionikeskus.ee/iii-sammas/vabatahtlikud-fondid/fid/81/" target="_blank"><?php _e('Previous reports', TEXT_DOMAIN) ?></a>
                             </li>
